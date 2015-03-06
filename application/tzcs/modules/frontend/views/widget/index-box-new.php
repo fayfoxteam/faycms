8db478a1debe2ce8757cfa9cc1e64fb6130ee467
whis <wqcsimple@foxmail.com>
2015-03-06 19:49:42 +0800

    修改测试通知文字显示长度

--- application/tzcs/modules/frontend/views/widget/index-box-new.php
+++ application/tzcs/modules/frontend/views/widget/index-box-new.php
@@ -6,7 +6,7 @@ use fay\helpers\String;
         	<div class="t1"><a href="<?php echo $this->url('cat/'.$data['top'])?>"><img src="<?php echo $this->staticFile('images/more.jpg')?>"></a><span><?php echo $data['title']?></span></div>
             <ul>
             <?php foreach ($posts as $p){?>
-            	<li><a href="<?php echo $this->url('post/'.$p['id'])?>" title="<?php echo $p['title']?>"><?php echo String::niceShort($p['title'], 18)?></a></li>
+            	<li><a href="<?php echo $this->url('post/'.$p['id'])?>" title="<?php echo $p['title']?>"><?php echo String::niceShort($p['title'], 26)?></a></li>
                 
                <?php }?>
             </ul>

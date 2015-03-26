e2a24931a2a7c8e23ab9cb7abb68683f89cf2b7f
whis <wqcismple@foxmail.com>
2015-03-26 23:11:44 +0800

    主页最新资讯去掉时间显示，修改css上下外边距

--- application/milk/modules/frontend/views/widget/new-news.php
+++ application/milk/modules/frontend/views/widget/new-news.php
@@ -8,7 +8,7 @@
                            <?php foreach ($posts as $post){?>
 
                             <li>
-                                <time datetime="<?php echo Date::format($post['publish_time'])?>"><?php echo Date::niceShort($post['publish_time'])?></time>
+                                
                                 <a href="<?php echo $this->url('post/'.$post['id'])?>"><?php echo $post['title']?></a>
                             </li>
                         <?php }?>

c8b3fe8d5d9f51bf020b32117d469a48b1bae4b9
whis <wqcismple@foxmail.com>
2015-03-31 21:36:22 +0800

    edit

--- application/vote/modules/frontend/views/index/index.php
+++ application/vote/modules/frontend/views/index/index.php
@@ -1,27 +1,15 @@
-<div class="row">
-<h5>候选人名单</h5>
+ 
 <hr width="100%" class="full-left" />
 
-<div class="col-md-3">
-<div class="panel panel-default">
-    <a href="" class="thumbnail">
-        <img src="<?php echo $this->staticFile('img/avatar.jpg')?>" alt="" />
-    </a>
-<div class="panel-body">
-    <div class="caption">
-        <h5>王琪琛</h5>
-        <p>1955.05外国语言文化与国际交流学院</p>
-        <p>
-        <div class="checkbox">
-            <label for="checkout"><input type="checkbox" name="checkout" id="checkout" /> 选择</label>
-            <a href="" class="btn btn-xs btn-info pull-right">查看详情</a>
-        </div>   
-        </p>
+<div class="row">
+<div class="row">
+    <div class="col-md-10 text-left"><h3>候选人名单</h3></div>
+    <div class="col-md-2">
+        <h5><button class="btn btn-danger pull-right" data-toggle="modal" data-target="#login-window" >登录进行投票</button></h5>
     </div>
- </div>   
 </div>
-</div>
-<div class="col-md-3">
+<div class="clear-10"></div>
+<div class="col-md-3 col-sm-6">
 <div class="panel panel-default">
     <a href="" class="thumbnail">
         <img src="<?php echo $this->staticFile('img/avatar.jpg')?>" alt="" />
@@ -32,45 +20,10 @@
         <p>1955.05外国语言文化与国际交流学院</p>
         <p>
         <div class="checkbox">
-            <label for="checkout"><input type="checkbox" name="checkout" id="checkout" /> 选择</label>
-            <a href="" class="btn btn-xs btn-info pull-right">查看详情</a>
-        </div>   
-        </p>
-    </div>
- </div>   
-</div>
-</div>
-<div class="col-md-3">
-<div class="panel panel-default">
-    <a href="" class="thumbnail">
-        <img src="<?php echo $this->staticFile('img/avatar.jpg')?>" alt="" />
-    </a>
-<div class="panel-body">
-    <div class="caption">
-        <h5>王琪琛</h5>
-        <p>1955.05外国语言文化与国际交流学院</p>
-        <p>
-        <div class="checkbox">
-            <label for="checkout"><input type="checkbox" name="checkout" id="checkout" /> 选择</label>
-            <a href="" class="btn btn-xs btn-info pull-right">查看详情</a>
-        </div>   
-        </p>
-    </div>
- </div>   
-</div>
-</div>
-<div class="col-md-3">
-<div class="panel panel-default">
-    <a href="" class="thumbnail">
-        <img src="<?php echo $this->staticFile('img/avatar.jpg')?>" alt="" />
-    </a>
-<div class="panel-body">
-    <div class="caption">
-        <h5>王琪琛</h5>
-        <p>1955.05外国语言文化与国际交流学院</p>
-        <p>
-        <div class="checkbox">
-            <label for="checkout"><input type="checkbox" name="checkout" id="checkout" /> 选择</label>
+            <label for="checkout">
+                <input type="checkbox" name="checkout" id="checkout" /> 
+                <span data-toggle="tooltip" data-placement="top" title="选我，选我">选择</span>
+            </label>
             <a href="" class="btn btn-xs btn-info pull-right">查看详情</a>
         </div>   
         </p>
@@ -79,160 +32,24 @@
 </div>
 </div>
 
-<div class="col-md-3">
-<div class="panel panel-default">
-    <a href="" class="thumbnail">
-        <img src="<?php echo $this->staticFile('img/avatar.jpg')?>" alt="" />
-    </a>
-<div class="panel-body">
-    <div class="caption">
-        <h5>王琪琛</h5>
-        <p>1955.05外国语言文化与国际交流学院</p>
-        <p>
-        <div class="checkbox">
-            <label for="checkout"><input type="checkbox" name="checkout" id="checkout" /> 选择</label>
-            <a href="" class="btn btn-xs btn-info pull-right">查看详情</a>
-        </div>   
-        </p>
-    </div>
- </div>   
-</div>
-</div>
-<div class="col-md-3">
-<div class="panel panel-default">
-    <a href="" class="thumbnail">
-        <img src="<?php echo $this->staticFile('img/avatar.jpg')?>" alt="" />
-    </a>
-<div class="panel-body">
-    <div class="caption">
-        <h5>王琪琛</h5>
-        <p>1955.05外国语言文化与国际交流学院</p>
-        <p>
-        <div class="checkbox">
-            <label for="checkout"><input type="checkbox" name="checkout" id="checkout" /> 选择</label>
-            <a href="" class="btn btn-xs btn-info pull-right">查看详情</a>
-        </div>   
-        </p>
-    </div>
- </div>   
-</div>
-</div>
-<div class="col-md-3">
-<div class="panel panel-default">
-    <a href="" class="thumbnail">
-        <img src="<?php echo $this->staticFile('img/avatar.jpg')?>" alt="" />
-    </a>
-<div class="panel-body">
-    <div class="caption">
-        <h5>王琪琛</h5>
-        <p>1955.05外国语言文化与国际交流学院</p>
-        <p>
-        <div class="checkbox">
-            <label for="checkout"><input type="checkbox" name="checkout" id="checkout" /> 选择</label>
-            <a href="" class="btn btn-xs btn-info pull-right">查看详情</a>
-        </div>   
-        </p>
-    </div>
- </div>   
-</div>
-</div>
-<div class="col-md-3">
-<div class="panel panel-default">
-    <a href="" class="thumbnail">
-        <img src="<?php echo $this->staticFile('img/avatar.jpg')?>" alt="" />
-    </a>
-<div class="panel-body">
-    <div class="caption">
-        <h5>王琪琛</h5>
-        <p>1955.05外国语言文化与国际交流学院</p>
-        <p>
-        <div class="checkbox">
-            <label for="checkout"><input type="checkbox" name="checkout" id="checkout" /> 选择</label>
-            <a href="" class="btn btn-xs btn-info pull-right">查看详情</a>
-        </div>   
-        </p>
-    </div>
- </div>   
-</div>
-</div>
-
-<div class="col-md-3">
-<div class="panel panel-default">
-    <a href="" class="thumbnail">
-        <img src="<?php echo $this->staticFile('img/avatar.jpg')?>" alt="" />
-    </a>
-<div class="panel-body">
-    <div class="caption">
-        <h5>王琪琛</h5>
-        <p>1955.05外国语言文化与国际交流学院</p>
-        <p>
-        <div class="checkbox">
-            <label for="checkout"><input type="checkbox" name="checkout" id="checkout" /> 选择</label>
-            <a href="" class="btn btn-xs btn-info pull-right">查看详情</a>
-        </div>   
-        </p>
-    </div>
- </div>   
-</div>
-</div>
-<div class="col-md-3">
-<div class="panel panel-default">
-    <a href="" class="thumbnail">
-        <img src="<?php echo $this->staticFile('img/avatar.jpg')?>" alt="" />
-    </a>
-<div class="panel-body">
-    <div class="caption">
-        <h5>王琪琛</h5>
-        <p>1955.05外国语言文化与国际交流学院</p>
-        <p>
-        <div class="checkbox">
-            <label for="checkout"><input type="checkbox" name="checkout" id="checkout" /> 选择</label>
-            <a href="" class="btn btn-xs btn-info pull-right">查看详情</a>
-        </div>   
-        </p>
-    </div>
- </div>   
-</div>
-</div>
-<div class="col-md-3">
-<div class="panel panel-default">
-    <a href="" class="thumbnail">
-        <img src="<?php echo $this->staticFile('img/avatar.jpg')?>" alt="" />
-    </a>
-<div class="panel-body">
-    <div class="caption">
-        <h5>王琪琛</h5>
-        <p>1955.05外国语言文化与国际交流学院</p>
-        <p>
-        <div class="checkbox">
-            <label for="checkout"><input type="checkbox" name="checkout" id="checkout" /> 选择</label>
-            <a href="" class="btn btn-xs btn-info pull-right">查看详情</a>
-        </div>   
-        </p>
-    </div>
- </div>   
-</div>
-</div>
-<div class="col-md-3">
-<div class="panel panel-default">
-    <a href="" class="thumbnail">
-        <img src="<?php echo $this->staticFile('img/avatar.jpg')?>" alt="" />
-    </a>
-<div class="panel-body">
-    <div class="caption">
-        <h5>王琪琛</h5>
-        <p>1955.05外国语言文化与国际交流学院</p>
-        <p>
-        <div class="checkbox">
-            <label for="checkout"><input type="checkbox" name="checkout" id="checkout" /> 选择</label>
-            <a href="" class="btn btn-xs btn-info pull-right">查看详情</a>
-        </div>   
-        </p>
-    </div>
- </div>   
-</div>
-</div>
 
 </div>
 
+<div class="modal fade" id="login-window" tabindex="-1" role="dialog" aria-labelledby="login-title" aria-hidden="true" >
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="login-title">登录</h4>
+            </div>
+            <div class="modal-body">
+            111
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+                <button type="button" class="btn btn-primary">登录</button>
+            </div>
+        </div>
+    </div>
+</div>
 <hr />
---
+++ application/vote/modules/frontend/views/layouts/_footer.php
@@ -0,0 +1,9 @@
+
+<nav class="footer">
+<div class="container">
+<div class="row">
+<p class="text-center">designer by whis...</p>
+</div>
+
+</div>
+</nav>

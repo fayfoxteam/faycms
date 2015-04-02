f6d797ab6af74e367b61a34fd37cc1f6001f89c8
whis <wqcismple@foxmail.com>
2015-04-02 21:04:27 +0800

    增加page页面和post页面，内页样式还没有写，估计清明回去写了

--- application/vote/modules/frontend/controllers/IndexController.php
+++ application/vote/modules/frontend/controllers/IndexController.php
@@ -2,10 +2,15 @@
 namespace vote\modules\frontend\controllers;
 
 use vote\library\FrontendController;
+use fay\models\Post;
+use fay\models\Page;
 class IndexController extends FrontendController
 {
     public function index()
     {
+        $this->view->introduce = Page::model()->getByAlias('introduce');
+        
+        $this->view->lists = Post::model()->getByCatAlias('list');
         
         $this->view->render();
     }
--- application/vote/modules/frontend/views/index/index.php
+++ application/vote/modules/frontend/views/index/index.php
@@ -1,4 +1,17 @@
- 
+<?php 
+use fay\helpers\Html;
+use fay\models\File;
+use fay\helpers\String;
+// dump($introduce);
+?> 
+
+
+<div class="container">
+<h1 class="text-center">教学名师评选 <small>评选截止时间: 2015年06月01日</small></h1>
+<p><?php echo Html::encode($introduce['abstract'])?><a href="<?php echo $this->url('page/'.$introduce['id'])?>" class="btn btn-xs btn-primary">查看详细</a></p>
+
+</div>
+
 <hr width="100%" class="full-left" />
 
 <div class="row">
@@ -9,44 +22,50 @@
     </div>
 </div>
 <div class="clear-10"></div>
+<?php foreach ($lists as $list ){?>
 <div class="col-md-3 col-sm-6">
 <div class="panel panel-default">
-    <a href="" class="thumbnail">
-        <img src="<?php echo $this->staticFile('img/avatar.jpg')?>" alt=""  data-toggle="tooltip" data-placement="bottom" title="点击照片查看详情"/>
+    <a href="<?php echo $this->url('post/'.$list['id'])?>" class="thumbnail">
+        <?php echo Html::img($list['id'], File::PIC_RESIZE, array('dw'=>251, 'dh' => 166, 'title'=> $list['title'], 'alt'=> $list['title'], 'data-toggle'=>"tooltip", 'data-placement'=>"bottom", 'title'=>"点击照片查看详情"))?>
     </a>
 <div class="panel-body">
     <div class="caption">
-        <h5>唐晓平</h5>
-        <p>
-        1955.05
-        外国语言文化与国际交流学院</p>
-        <p>
+        <h5><?php echo $list['title']?></h5>
+        <p><?php echo $list['abstract']?></p>
+       
         <div class="checkbox">
-            <label for="checkout">
-                <input type="checkbox" name="checkout" id="checkout" /> 
+            <label for="data-id-<?php echo $list['id']?>">
+                <input type="checkbox" name="checkout" id="data-id-<?php echo $list['id']?>" /> 
                 <span data-toggle="tooltip" data-placement="top" title="选唐晓平，唐晓平，晓平，平。。come on baby">选择</span>
             </label>
-            <a href="" class="btn btn-xs btn-info pull-right" data-toggle="tooltip" data-placement="top" title="点我查看详情">查看详情</a>
+            <a href="<?php echo $this->url('post/'.$list['id'])?>" class="btn btn-xs btn-info pull-right" data-toggle="tooltip" data-placement="top" title="点我查看详情">查看详情</a>
         </div>   
         </p>
     </div>
  </div>   
 </div>
 </div>
-
+<?php }?>
 
 
 </div>
 
 <div class="modal fade" id="login-window" tabindex="-1" role="dialog" aria-labelledby="login-title" aria-hidden="true" >
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-sm">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title" id="login-title">登录</h4>
             </div>
             <div class="modal-body">
-            111
+                <div class="form-group">
+                    <label for="username">用户名:</label>
+                    <input type="text" class="form-control" id="username" name="username" />
+                </div>
+                <div class="form-group">
+                    <label for="password">密码:</label>
+                    <input type="password" name="password" id="password" class="form-control" />
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>

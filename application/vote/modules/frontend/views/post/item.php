0b90aaecb756e1ceb4dc1153ac5cd929bee85229
whis <wqcismple@foxmail.com>
2015-04-03 20:46:13 +0800

    修改内页显示样式

--- application/vote/modules/frontend/views/post/item.php
+++ application/vote/modules/frontend/views/post/item.php
@@ -68,44 +68,57 @@ if (count($posts['props']) > 0)
         </div>
     </div>
     <div class="container">
-        <div class="row">
-            <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample1" aria-expanded="false" aria-controls="collapseExample">
-                教学理念
-            </a>
-            <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample2" aria-expanded="false" aria-controls="collapseExample">
-                教学品德
-            </a>
-            <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample3" aria-expanded="false" aria-controls="collapseExample">
-                教学改进
-            </a>
+        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingOne">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                           教学理念
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                    <div class="panel-body">
+                        【教学理念】：以博雅教育为体，素质教育为本，知识传播与能力训练相结合，润物无声地培养学生的批判思维能力，以达到培养学生在学习过程中自我形成创造性思维习惯的目的。在担任外语学院常务副院长期间提出.
+                        <a href="" class="btn btn-xs btn-success">更多>></a>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingTwo">
+                    <h4 class="panel-title">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            教学品德
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                    <div class="panel-body">
+                        本人以学生人生发展为教育、教学为基本出发点，不仅在课堂上传授知识，更重视学生大学期间所学知识在职业发展和人生道路中的潜力， 以及这些知识的生成和对社会的贡献，多年来，本人不仅在课堂上树立学科...
+                        <a href="" class="btn btn-xs btn-success">更多>></a>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingThree">
+                    <h4 class="panel-title">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            教学改进
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                    <div class="panel-body">
+                        本人带领德语语言文学专业教学团队取得优异的教学改革成果，形成了德语专业模块教学、交叉学科课程体系， 国际化教学特色，受到学校多次表彰，浙江大学报对本专业的国际化办学特色进行过专版报道。本人.
+                        <a href="" class="btn btn-xs btn-success">更多>></a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
-    <div class="clear-10"></div>
-    <div class="collapse" id="collapseExample1">
-        <div class="well">
-            <p>
-                【教学理念】：以博雅教育为体，素质教育为本，知识传播与能力训练相结合，润物无声地培养学生的批判思维能力，以达到培养学生在学习过程中自我形成创造性思维习惯的目的。在担任外语学院常务副院长期间提出
-            </p>
-            <a href="" class="btn btn-xs btn-success">查看更多>></a>
-        </div>
-    </div>
-    <div class="collapse" id="collapseExample2">
-        <div class="well">
-            <p>
-                人以学生人生发展为教育、教学为基本出发点，不仅在课堂上传授知识，更重视学生大学期间所学知识在职业发展和人生道路中的潜力， 以及这些知识的生成和对社会的贡献，多年来，本人不仅在课堂上树立学科
-            </p>
-            <a href="" class="btn btn-xs btn-success">查看更多>></a>
-        </div>
-    </div>
-    <div class="collapse" id="collapseExample3">
-        <div class="well">
-            <p>
-                本人带领德语语言文学专业教学团队取得优异的教学改革成果，形成了德语专业模块教学、交叉学科课程体系， 国际化教学特色，受到学校多次表彰，浙江大学报对本专业的国际化办学特色进行过专版报道。本人.
-            </p>
-            <a href="" class="btn btn-xs btn-success">查看更多>></a>
-        </div>
-    </div>
+
+
 
     <div class="clear-30"></div>
     <div class="container">

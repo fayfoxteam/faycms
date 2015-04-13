d6c2eef8d50facce2dbf4e5a67a189eeebaae787
whis <wqcismple@foxmail.com>
2015-04-13 23:36:12 +0800

    设计投票结果页面

---
+++ application/vote/modules/admin/views/vote/detail.php
@@ -0,0 +1,48 @@
+
+<style>
+#DIV_1 {
+    box-shadow: rgba(0, 0, 0, 0.0980392) 0px 1px 2px 0px inset;
+    box-sizing: border-box;
+    color: rgb(51, 51, 51);
+    height: 20px;
+    width: 100%;
+    perspective-origin: 407.75px 10px;
+    transform-origin: 407.75px 10px;
+    background: rgb(245, 245, 245) none repeat scroll 0% 0% / auto padding-box border-box;
+    border: 0px none rgb(51, 51, 51);
+    border-radius: 4px 4px 4px 4px;
+    font: normal normal normal normal 14px/20px 'Helvetica Neue', Helvetica, 'Microsoft Yahei', 'Hiragino Sans GB', 'WenQuanYi Micro Hei', sans-serif;
+    outline: rgb(51, 51, 51) none 0px;
+    overflow: hidden;
+}/*#DIV_1*/
+
+#DIV_2 {
+    box-shadow: rgba(0, 0, 0, 0.14902) 0px -1px 0px 0px inset;
+    box-sizing: border-box;
+    color: rgb(255, 255, 255);
+    float: left;
+    height: 20px;
+    text-align: center;
+    perspective-origin: 244.640625px 10px;
+    transform-origin: 244.640625px 10px;
+    background: rgb(51, 122, 183) none repeat scroll 0% 0% / auto padding-box border-box;
+    border: 0px none rgb(255, 255, 255);
+    font: normal normal normal normal 12px/20px 'Helvetica Neue', Helvetica, 'Microsoft Yahei', 'Hiragino Sans GB', 'WenQuanYi Micro Hei', sans-serif;
+    outline: rgb(255, 255, 255) none 0px;
+    transition: width 0.6s ease 0s;
+}/*#DIV_2*/
+
+
+</style>
+
+<div class="col-12">
+<div class="row">
+    <h3>whis</h3>
+    <div id="DIV_1">
+	<div id="DIV_2" style="width: 10%">
+		60%
+	</div>
+</div>
+</div>
+
+</div>

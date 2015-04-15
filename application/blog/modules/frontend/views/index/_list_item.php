f7e6d0b130ea578861eb616f7b876ca597ccc002
whis <wqcismple@foxmail.com>
2015-04-15 22:21:40 +0800

    delete other application

--- application/blog/modules/frontend/views/index/_list_item.php
+++
@@ -1,8 +0,0 @@
-<?php
-use fay\models\Category;
-
-if(Category::model()->isChild($data['cat_id'], $work_cat)){
-	$this->renderPartial('index/_work_item', array('data'=>$data));
-}else{
-	$this->renderPartial('index/_post_item', array('data'=>$data));
-}
--- public/static/blog/js/menu.js
+++
@@ -1,9 +0,0 @@
-jQuery.fn.extend({
-	menu: function(){
-		$(this).children("li").each(function(key){
-			$(this).prepend('<span class="menu-l pngbg"></span>')
-				.append('<span class="menu-r pngbg"></span>')
-				.children("a").wrap('<span class="menu-m pngbg"></span>')
-		})
-	}
-})

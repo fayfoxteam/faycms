7c6a0d5c0cf51e4e362f78bae356d57cbdb3c412
whis <wqcsimple@foxmail.com>
2015-04-20 11:02:10 +0800

    修改首页title，keywords, description

--- application/vote/modules/frontend/views/layouts/main.php
+++ application/vote/modules/frontend/views/layouts/main.php
@@ -1,10 +1,33 @@
+<?php 
+use fay\models\Option;
+use fay\helpers\Html;
+?>
 <!doctype html>
 <html lang="zh-CN">
 <head>
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>教学名师评选</title>
+	<title>
+    <?php 
+        if (!empty($title)){
+            echo $title . ' - ';
+        }
+        echo Option::get('sitename');
+    ?>
+    </title>
+    <meta name="description" content="<?php if ($description !== ''){
+                                                echo Html::encode($description);
+                                                }else {
+                                                    echo Option::get('seo_index_description');
+                                                }      
+                                            ?>">
+    <meta name="keywords" content="<?php 
+									if($keywords !== ''){
+										echo Html::encode($keywords);
+									}else{
+										echo Option::get('seo_index_keywords');
+									}?>"/>
 	<link rel="stylesheet" href="<?php echo $this->staticFile('css/bootstrap.min.css')?>" />
 	<link rel="stylesheet" href="<?php echo $this->staticFile('css/style.css')?>" />
     <script type="text/javascript" src="<?php echo $this->staticFile('js/jquery.1.11.1.js')?>"></script>

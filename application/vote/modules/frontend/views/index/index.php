0840430e588c7e4e6d80f5f100b766ed8846ea3d
whis <wqcsimple@foxmail.com>
2015-04-20 10:24:51 +0800

    fix bug

--- application/vote/modules/frontend/views/index/index.php
+++ application/vote/modules/frontend/views/index/index.php
@@ -17,7 +17,7 @@ $redis->connect('redis', 6379, 300);
             <img src="<?php echo $this->staticFile('/img/logo.gif') ?>" width="215" height="37" alt=""/>
         </div>
         <div class="col-md-9">
-            <h2 class="text-left"><?php F::widget()->get('index-title') ?></h2>
+            <h2 class="text-left"><?php F::widget()->load('index-title') ?></h2>
         </div>
     </div>
 

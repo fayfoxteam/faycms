7c14e601a66c39eba1375e16d01c5b3a105d6df3
whis <wqcsimple@foxmail.com>
2015-04-30 15:26:39 +0800

    修改选专业为投票系统

--- application/vote/modules/frontend/views/index/index.php
+++ application/vote/modules/frontend/views/index/index.php
@@ -30,7 +30,7 @@ $redis->connect('redis', 6379, 300);
 
 <div class="row">
 <div class="row">
-    <div class="col-md-8 col-sm-6 col-xs-5 col-xs-offset-1 text-center"></div>
+    <div class="col-md-8 col-sm-6 col-xs-5 col-xs-offset-1 text-center"><h4>候选人名单</h4></div>
     <div class="col-md-3 col-sm-5 col-xs-5 text-right">
         <h5>
             <?php if (F::app()->session->get('id')){ ?>
@@ -45,11 +45,13 @@ $redis->connect('redis', 6379, 300);
 </div>
 <div class="clear-10"></div>
 <?php
-
+shuffle($lists);
  foreach ($lists as $list ){?>
 <div class="col-md-3 col-sm-6 teach-list">
 <div class="panel panel-default panel-until">
-    
+    <a href="<?php echo $this->url('post/'.$list['id'])?>" class="thumbnail">
+        <?php echo Html::img($list['thumbnail'], File::PIC_ORIGINAL, array('width'=>'100%', 'title'=> $list['title'], 'alt'=> $list['title'], 'data-toggle'=>"tooltip", 'data-placement'=>"bottom", 'title'=>"点击照片查看详情"))?>
+    </a>
 <div class="panel-body">
     <div class="caption">
         <h5><?php echo $list['title']?></h5>
@@ -57,9 +59,10 @@ $redis->connect('redis', 6379, 300);
        
         <div class="checkbox">
             <label for="data-id-<?php echo $list['id']?>">
-                <input type="radio" name="checkout" class="checked" id="data-id-<?php echo $list['id'] ?>" data-id="<?php echo $list['id']?>" />
-                <span data-toggle="tooltip" data-placement="top" title="点击选择<?php echo $list['title'] ?>">选择</span>
+                <input type="checkbox" name="checkout" class="checked" id="data-id-<?php echo $list['id'] ?>" data-id="<?php echo $list['id']?>" />
+                <span data-toggle="tooltip" data-placement="top" title="点击选择<?php echo $list['title'] ?>老师">选择</span>
             </label>
+            <a href="<?php echo $this->url('post/'.$list['id'])?>" class="btn btn-xs btn-info pull-right">查看详情</a>
         </div>   
         </p>
     </div>
@@ -86,7 +89,7 @@ $redis->connect('redis', 6379, 300);
                ?>
                <div class="btn btn-danger form-control" disabled>您已经投过了，只有一次机会的哦</div>
         <?php }else { ?>
-            <div class="btn btn-primary form-control" id="vote_submit">完成选择</div>
+            <div class="btn btn-primary form-control" id="vote_submit">投票</div>
         <?php } } ?>
    
 </div>
@@ -104,13 +107,11 @@ $redis->connect('redis', 6379, 300);
             <div class="modal-body">
                 <div class="form-group">
                     <label for="username">用户名:</label>
-                    <input type="text" class="form-control" id="username" name="username" />
-                    <p class="help-block">学生为学号，老师为工号</p>
+                    <input type="text" class="form-control" id="username" name="username" placeholder="学生为学号，老师为工号"/>
                 </div>
                 <div class="form-group">
                     <label for="password">密码:</label>
-                    <input type="password" name="password" id="password" class="form-control" />
-                    <p class="help-block">密码为身份证后6位，或者为12345</p>
+                    <input type="password" name="password" id="password" class="form-control" placeholder="密码为身份证后6位，或者为12345"/>
                 </div>
                 <div class="form-group">
                     <label for="vcode">验证码</label>

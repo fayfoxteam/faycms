efd276c1c8611828a4b5dd9d0cf72b9e3799a93f
fayfox <fayfox@duomai.com>
2015-06-09 13:10:23 +0800

    修改doc项目配置文件

--- application/doc/configs/main.php
+++ application/doc/configs/main.php
@@ -9,8 +9,8 @@ return array(
 	 */
 	'db'=>array(
 		'host'=>'localhost',					//数据库服务器
-		'user'=>'faycms',							//用户名
-		'password'=>'jDoBjHwVq6q2hQVN',							//密码
+		'user'=>$_SERVER['HTTP_HOST'] == 'doc.faycms.com' ? 'faycms' : 'root',//用户名
+		'password'=>$_SERVER['HTTP_HOST'] == 'doc.faycms.com' ? 'jDoBjHwVq6q2hQVN' : '',//密码
 		'port'=>3306,							//端口
 		'dbname'=>'faycms_doc',					//数据库名
 		'charset'=>'utf8',						//数据库编码方式

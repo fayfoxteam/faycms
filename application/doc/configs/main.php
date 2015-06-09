e38135d7c2e0f352bbabfa6196a9df38ed64622f
fayfox <fayfox@AY130930133745801978Z.(none)>
2015-06-09 13:03:43 +0800

    修改远程主机数据库链接方式

--- application/doc/configs/main.php
+++ application/doc/configs/main.php
@@ -9,10 +9,10 @@ return array(
 	 */
 	'db'=>array(
 		'host'=>'localhost',					//数据库服务器
-		'user'=>'root',							//用户名
-		'password'=>'',							//密码
+		'user'=>'faycms',							//用户名
+		'password'=>'jDoBjHwVq6q2hQVN',							//密码
 		'port'=>3306,							//端口
-		'dbname'=>'fayfox_doc',					//数据库名
+		'dbname'=>'faycms_doc',					//数据库名
 		'charset'=>'utf8',						//数据库编码方式
 		'table_prefix'=>'fayfox_',				//数据库表前缀
 	),

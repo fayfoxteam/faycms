9cf83e85456797252ec31fae00c5ece08f8d78f4
fayfox <admin@fayfox.com>
2015-06-11 21:21:45 +0800

    修改blog的数据库配置

--- application/blog/configs/main.php
+++ application/blog/configs/main.php
@@ -9,10 +9,10 @@ return array(
 	 */
 	'db'=>array(
 		'host'=>'localhost',					//数据库服务器
-		'user'=>'root',							//用户名
-		'password'=>'',							//密码
+		'user'=>$_SERVER['HTTP_HOST'] == 'doc.faycms.com' ? 'faycms' : 'root',//用户名
+		'password'=>$_SERVER['HTTP_HOST'] == 'doc.faycms.com' ? 'jDoBjHwVq6q2hQVN' : '',//密码
 		'port'=>3306,							//端口
-		'dbname'=>'fayfox_blog',				//数据库名
+		'dbname'=>'faycms_blog',				//数据库名
 		'charset'=>'utf8',						//数据库编码方式
 		'table_prefix'=>'fayfox_',				//数据库表前缀
 	),

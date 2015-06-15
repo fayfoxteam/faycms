f9a33ab1c9e98c06408715329a92783d69f18ab7
fayfox <fayfox@haimi.com>
2015-06-15 17:23:47 +0800

    blog配置文件

--- application/blog/configs/main.php
+++ application/blog/configs/main.php
@@ -9,8 +9,8 @@ return array(
 	 */
 	'db'=>array(
 		'host'=>'localhost',					//数据库服务器
-		'user'=>$_SERVER['HTTP_HOST'] == 'doc.faycms.com' ? 'faycms' : 'root',//用户名
-		'password'=>$_SERVER['HTTP_HOST'] == 'doc.faycms.com' ? 'jDoBjHwVq6q2hQVN' : '',//密码
+		'user'=>$_SERVER['HTTP_HOST'] == 'blog.faycms.com' ? 'faycms' : 'root',//用户名
+		'password'=>$_SERVER['HTTP_HOST'] == 'blog.faycms.com' ? 'jDoBjHwVq6q2hQVN' : '',//密码
 		'port'=>3306,							//端口
 		'dbname'=>'faycms_blog',				//数据库名
 		'charset'=>'utf8',						//数据库编码方式

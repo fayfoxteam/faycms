0d4b505e70f0b1b61ff2408a7a3e8bd752b0cfa7
fayfox <fayfox@haimi.com>
2015-07-17 18:02:39 +0800

    后台云之讯配置

--- vendor/cms/modules/admin/views/site/settings.php
+++ vendor/cms/modules/admin/views/site/settings.php
@@ -5,6 +5,7 @@
 				<li class="active"><a href="#options-panel">站点参数</a></li>
 				<li><a href="#email-panel">邮箱参数</a></li>
 				<li><a href="#qiniu-panel">七牛参数</a></li>
+				<li><a href="#ucpaas-panel">云之讯参数</a></li>
 			</ul>
 			<div class="tab-content">
 				<div id="options-panel" class="tab-pane p5">
@@ -15,6 +16,9 @@
 				</div>
 				<div id="qiniu-panel" class="tab-pane p5 hide">
 					<?php $this->renderPartial('_qiniu_panel')?>
+				</div>
+				<div id="ucpaas-panel" class="tab-pane p5 hide">
+					<?php $this->renderPartial('_ucpaas_panel')?>
 				</div>
 			</div>
 		</div>

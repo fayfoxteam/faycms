77d1f5ea8a56a17debb9636813474860b5cfbecc
fayfox <fayfox@haimi.com>
2015-07-20 14:15:35 +0800

    改了下fay\core\Response::json的格式

--- vendor/fay/core/Response.php
+++ vendor/fay/core/Response.php
@@ -209,13 +209,13 @@ class Response{
 	 * @param string $error_code 错误码。用有意义的英文描述组成，但不是给人看的，是给程序确定错误用的。例如：username:can-not-be-empty
 	 * @param string $error_message 错误描述。人类可读的描述，一般用于弹窗报错，例如：用户名不能为空！
 	 */
-	public static function json($content, $status = 1, $error_message = '', $error_code = ''){
+	public static function json($content, $status = 1, $message = '', $code = ''){
 		header('Content-Type:application/json; charset=utf-8');
 		echo json_encode(array(
 			'status'=>$status == 0 ? 0 : 1,
 			'content'=>$content,
-			'errCode'=>$error_code,
-			'errMsg'=>$error_message
+			'code'=>$code,
+			'message'=>$message
 		));
 		die;
 	}

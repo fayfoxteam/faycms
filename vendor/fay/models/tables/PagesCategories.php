d952fe1e3faf138dd2f14cdb2783cdcd58f6a430
fayfox <admin@fayfox.com>
2015-07-21 23:14:06 +0800

    换行符问题

--- vendor/fay/models/tables/PagesCategories.php
+++ vendor/fay/models/tables/PagesCategories.php
@@ -1,37 +1,37 @@
-<?php
-namespace fay\models\tables;
-
-use fay\core\db\Table;
-
-class PagesCategories extends Table{
-	protected $_name = 'pages_categories';
-	protected $_primary = array('page_id', 'cat_id');
-	
-	/**
-	 * @return PagesCategories
-	 */
-	public static function model($className=__CLASS__){
-		return parent::model($className);
-	}
-	
-	public function rules(){
-		return array(
-			array(array('page_id'), 'int', array('min'=>0, 'max'=>4294967295)),
-			array(array('cat_id'), 'int', array('min'=>0, 'max'=>16777215)),
-		);
-	}
-
-	public function labels(){
-		return array(
-			'page_id'=>'Page Id',
-			'cat_id'=>'Cat Id',
-		);
-	}
-
-	public function filters(){
-		return array(
-			'page_id'=>'intval',
-			'cat_id'=>'intval',
-		);
-	}
+<?php
+namespace fay\models\tables;
+
+use fay\core\db\Table;
+
+class PagesCategories extends Table{
+	protected $_name = 'pages_categories';
+	protected $_primary = array('page_id', 'cat_id');
+	
+	/**
+	 * @return PagesCategories
+	 */
+	public static function model($className=__CLASS__){
+		return parent::model($className);
+	}
+	
+	public function rules(){
+		return array(
+			array(array('page_id'), 'int', array('min'=>0, 'max'=>4294967295)),
+			array(array('cat_id'), 'int', array('min'=>0, 'max'=>16777215)),
+		);
+	}
+
+	public function labels(){
+		return array(
+			'page_id'=>'Page Id',
+			'cat_id'=>'Cat Id',
+		);
+	}
+
+	public function filters(){
+		return array(
+			'page_id'=>'intval',
+			'cat_id'=>'intval',
+		);
+	}
 }
--- vendor/fay/models/tables/Widgets.php
+++ vendor/fay/models/tables/Widgets.php
@@ -1,51 +1,51 @@
-<?php
-namespace fay\models\tables;
-
-use fay\core\db\Table;
-
-class Widgets extends Table{
-	protected $_name = 'widgets';
-	
-	/**
-	 * @return Widgets
-	 */
-	public static function model($className=__CLASS__){
-		return parent::model($className);
-	}
-	
-	public function rules(){
-		return array(
-			array(array('id'), 'int', array('min'=>0, 'max'=>65535)),
-			array(array('enabled'), 'int', array('min'=>-128, 'max'=>127)),
-			array(array('sort'), 'int', array('min'=>0, 'max'=>255)),
-			array(array('alias', 'widget_name', 'description'), 'string', array('max'=>255)),
-			array(array('widgetarea'), 'string', array('max'=>50)),
-		);
-	}
-
-	public function labels(){
-		return array(
-			'id'=>'Id',
-			'alias'=>'别名',
-			'options'=>'实例参数',
-			'widget_name'=>'小工具名称',
-			'description'=>'小工具描述',
-			'enabled'=>'是否启用',
-			'widgetarea'=>'小工具域',
-			'sort'=>'排序值',
-		);
-	}
-
-	public function filters(){
-		return array(
-			'id'=>'intval',
-			'alias'=>'trim',
-			'options'=>'',
-			'widget_name'=>'trim',
-			'description'=>'trim',
-			'enabled'=>'intval',
-			'widgetarea'=>'trim',
-			'sort'=>'intval',
-		);
-	}
+<?php
+namespace fay\models\tables;
+
+use fay\core\db\Table;
+
+class Widgets extends Table{
+	protected $_name = 'widgets';
+	
+	/**
+	 * @return Widgets
+	 */
+	public static function model($className=__CLASS__){
+		return parent::model($className);
+	}
+	
+	public function rules(){
+		return array(
+			array(array('id'), 'int', array('min'=>0, 'max'=>65535)),
+			array(array('enabled'), 'int', array('min'=>-128, 'max'=>127)),
+			array(array('sort'), 'int', array('min'=>0, 'max'=>255)),
+			array(array('alias', 'widget_name', 'description'), 'string', array('max'=>255)),
+			array(array('widgetarea'), 'string', array('max'=>50)),
+		);
+	}
+
+	public function labels(){
+		return array(
+			'id'=>'Id',
+			'alias'=>'别名',
+			'options'=>'实例参数',
+			'widget_name'=>'小工具名称',
+			'description'=>'小工具描述',
+			'enabled'=>'是否启用',
+			'widgetarea'=>'小工具域',
+			'sort'=>'排序值',
+		);
+	}
+
+	public function filters(){
+		return array(
+			'id'=>'intval',
+			'alias'=>'trim',
+			'options'=>'',
+			'widget_name'=>'trim',
+			'description'=>'trim',
+			'enabled'=>'intval',
+			'widgetarea'=>'trim',
+			'sort'=>'intval',
+		);
+	}
 }

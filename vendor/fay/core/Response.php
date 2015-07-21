369cce39ef335285f4d38d6041fbb2eac10fb564
nginx <334220235@qq.com>
2015-07-21 17:09:57 +0800

    添加jsonp返回方法

--- vendor/fay/core/Response.php
+++ vendor/fay/core/Response.php
@@ -219,4 +219,28 @@ class Response{
 		));
 		die;
 	}
+
+
+	/**
+	 * 返回jsonp
+	 * @param int $status 1代表成功，0代表失败。（无其它状态，错误描述放$error_code）
+	 * @param mix $content 内容部分
+	 * @param string $error_code 错误码。用有意义的英文描述组成，但不是给人看的，是给程序确定错误用的。例如：username:can-not-be-empty
+	 * @param string $error_message 错误描述。人类可读的描述，一般用于弹窗报错，例如：用户名不能为空！
+	 * @param string $func jsonp请求的回调函数名，在调用的地方，从请求中获取，例如jquery发送的请求：$func = $this->input->get('callback');！
+	 */
+	public static function jsonp($content, $status = 1, $message = '', $code = '',$func = 'callback'){
+		// 返回JSON数据格式到客户端 包含状态信息
+		header('Content-Type:application/json; charset=utf-8');
+		echo $func.'('.json_encode(array(
+			'status'=>$status == 0 ? 0 : 1,
+			'content'=>$content,
+			'code'=>$code,
+			'message'=>$message
+		)).');';
+		die;
+	}
+
+
+
 }

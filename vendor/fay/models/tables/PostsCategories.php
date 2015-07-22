89957a6fc2a6f21fcc66fe53cb476e8bd99d18d2
fayfox <fayfox@haimi.com>
2015-07-22 18:04:00 +0800

    换行符问题

--- vendor/fay/models/tables/PostsCategories.php
+++ vendor/fay/models/tables/PostsCategories.php
@@ -1,37 +1,37 @@
-<?php
-namespace fay\models\tables;
-
-use fay\core\db\Table;
-
-class PostsCategories extends Table{
-	protected $_name = 'posts_categories';
-	protected $_primary = array('post_id', 'cat_id');
-	
-	/**
-	 * @return PostsCategories
-	 */
-	public static function model($className=__CLASS__){
-		return parent::model($className);
-	}
-	
-	public function rules(){
-		return array(
-			array(array('post_id'), 'int', array('min'=>0, 'max'=>4294967295)),
-			array(array('cat_id'), 'int', array('min'=>0, 'max'=>16777215)),
-		);
-	}
-
-	public function labels(){
-		return array(
-			'post_id'=>'Post Id',
-			'cat_id'=>'Cat Id',
-		);
-	}
-
-	public function filters(){
-		return array(
-			'post_id'=>'intval',
-			'cat_id'=>'intval',
-		);
-	}
+<?php
+namespace fay\models\tables;
+
+use fay\core\db\Table;
+
+class PostsCategories extends Table{
+	protected $_name = 'posts_categories';
+	protected $_primary = array('post_id', 'cat_id');
+	
+	/**
+	 * @return PostsCategories
+	 */
+	public static function model($className=__CLASS__){
+		return parent::model($className);
+	}
+	
+	public function rules(){
+		return array(
+			array(array('post_id'), 'int', array('min'=>0, 'max'=>4294967295)),
+			array(array('cat_id'), 'int', array('min'=>0, 'max'=>16777215)),
+		);
+	}
+
+	public function labels(){
+		return array(
+			'post_id'=>'Post Id',
+			'cat_id'=>'Cat Id',
+		);
+	}
+
+	public function filters(){
+		return array(
+			'post_id'=>'intval',
+			'cat_id'=>'intval',
+		);
+	}
 }

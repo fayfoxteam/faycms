2b3daa35e56dc2daf29b70b17ef561dacc3a1ca0
fayfox <admin@fayfox.com>
2015-08-06 23:23:19 +0800

    准备将用户表拆成2张，并支持一个用户对应多个角色

---
+++ vendor/fay/models/tables/UserExtend.php
@@ -0,0 +1,75 @@
+<?php
+namespace fay\models\tables;
+
+use fay\core\db\Table;
+
+/**
+ * User Extend model
+ * 
+ * @property int $user_id
+ * @property int $reg_time
+ * @property int $reg_ip
+ * @property int $login_times
+ * @property int $last_login_time
+ * @property int $last_login_ip
+ * @property int $last_time_online
+ * @property string $trackid
+ * @property string $refer
+ * @property string $se
+ * @property string $keywords
+ */
+class UserExtend extends Table{
+	protected $_name = 'user_extend';
+	protected $_primary = 'user_id';
+	
+	/**
+	 * @return UserExtend
+	 */
+	public static function model($className=__CLASS__){
+		return parent::model($className);
+	}
+	
+	public function rules(){
+		return array(
+			array(array('last_login_ip'), 'int', array('min'=>-2147483648, 'max'=>2147483647)),
+			array(array('user_id', 'reg_ip'), 'int', array('min'=>0, 'max'=>4294967295)),
+			array(array('login_times'), 'int', array('min'=>0, 'max'=>65535)),
+			array(array('trackid'), 'string', array('max'=>50)),
+			array(array('refer', 'keywords'), 'string', array('max'=>255)),
+			array(array('se'), 'string', array('max'=>30)),
+			array(array('reg_time', 'last_login_time', 'last_time_online'), 'datetime'),
+		);
+	}
+
+	public function labels(){
+		return array(
+			'user_id'=>'用户ID',
+			'reg_time'=>'注册时间',
+			'reg_ip'=>'注册IP',
+			'login_times'=>'登录次数',
+			'last_login_time'=>'最后登录时间',
+			'last_login_ip'=>'最后登录IP',
+			'last_time_online'=>'最后在线时间',
+			'trackid'=>'追踪ID',
+			'refer'=>'来源URL',
+			'se'=>'搜索引擎',
+			'keywords'=>'搜索关键词',
+		);
+	}
+
+	public function filters(){
+		return array(
+			'user_id'=>'intval',
+			'reg_time'=>'trim',
+			'reg_ip'=>'intval',
+			'login_times'=>'trim',
+			'last_login_time'=>'trim',
+			'last_login_ip'=>'intval',
+			'last_time_online'=>'trim',
+			'trackid'=>'trim',
+			'refer'=>'trim',
+			'se'=>'trim',
+			'keywords'=>'trim',
+		);
+	}
+}
--- vendor/fay/models/tables/Users.php
+++ vendor/fay/models/tables/Users.php
@@ -3,6 +3,22 @@ namespace fay\models\tables;
 
 use fay\core\db\Table;
 
+/**
+ * Users model
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $email
+ * @property string $mobile
+ * @property string $password
+ * @property string $salt
+ * @property string $nickname
+ * @property int $avatar
+ * @property int $status
+ * @property int $block
+ * @property int $parent
+ * @property int $deleted
+ */
 class Users extends Table{
 	/**
 	 * 用户信息不完整
@@ -35,31 +51,6 @@ class Users extends Table{
 	const STATUS_NOT_VERIFIED = 4;
 
 	/**
-	 * 普通用户
-	 * @var int
-	 */
-	const ROLE_USER = 1;
-
-	/**
-	 * 账单用户（定制化需求）
-	 * @var int
-	 */
-	const ROLE_BILL = 2;
-
-	/**
-	 * 系统
-	 * @var int
-	 */
-	const ROLE_SYSTEM = 100;
-	
-	/**
-	 * 超级管理员<br>
-	 * 100以上的角色为管理员角色
-	 * @var int
-	 */
-	const ROLE_SUPERADMIN = 101;
-	
-	/**
 	 * 系统用户
 	 */
 	const ITEM_SYSTEM = 1;
@@ -85,23 +76,17 @@ class Users extends Table{
 	
 	public function rules(){
 		return array(
-			array(array('reg_ip', 'last_login_ip'), 'int', array('min'=>-2147483648, 'max'=>2147483647)),
-			array(array('id', 'avatar', 'last_login_time', 'last_time_online'), 'int', array('min'=>0, 'max'=>4294967295)),
+			array(array('id', 'avatar'), 'int', array('min'=>0, 'max'=>4294967295)),
 			array(array('parent'), 'int', array('min'=>0, 'max'=>16777215)),
-			array(array('login_times'), 'int', array('min'=>0, 'max'=>65535)),
 			array(array('status', 'block'), 'int', array('min'=>-128, 'max'=>127)),
-			array(array('role'), 'int', array('min'=>0, 'max'=>255)),
-			array(array('username', 'realname', 'nickname', 'trackid'), 'string', array('max'=>50)),
+			array(array('username', 'nickname'), 'string', array('max'=>50)),
 			array(array('password'), 'string', array('max'=>32)),
 			array(array('salt'), 'string', array('max'=>5)),
-			array(array('refer', 'keywords'), 'string', array('max'=>255)),
-			array(array('se'), 'string', array('max'=>30)),
 			array(array('deleted'), 'range', array('range'=>array('0', '1'))),
-			array(array('cellphone'), 'mobile'),
+			array(array('mobile'), 'mobile'),
 
 			array(array('username'), 'unique', array('on'=>'create', 'table'=>'users', 'field'=>'username', 'ajax'=>array('tools/user/is-username-not-exist'))),
 			array(array('username'), 'unique', array('on'=>'edit', 'table'=>'users', 'field'=>'username', 'except'=>'id', 'ajax'=>array('tools/user/is-username-not-exist'))),
-			array(array('refer'), 'url'),
 			array(array('email'), 'email'),
 		);
 	}
@@ -111,59 +96,32 @@ class Users extends Table{
 			'id'=>'Id',
 			'username'=>'登录名',
 			'email'=>'邮箱',
-			'cellphone'=>'手机号码',
+			'mobile'=>'手机号码',
 			'password'=>'密码',
 			'salt'=>'五位随机数',
-			'realname'=>'用户名',
 			'nickname'=>'昵称',
-			'professional'=>'专业',
-			'sex'=>'性别',
 			'avatar'=>'头像',
-			'reg_time'=>'注册时间',
-			'reg_ip'=>'注册ip',
-			'login_times'=>'登陆次数',
-			'last_login_time'=>'最后登陆时间',
-			'last_login_ip'=>'最后登陆者ip',
-			'last_time_online'=>'最后在线时间',
 			'status'=>'用户审核状态',
 			'block'=>'屏蔽用户',
-			'role'=>'角色',
 			'parent'=>'父节点',
 			'deleted'=>'Deleted',
-			'trackid'=>'Trackid',
-			'refer'=>'来源URL',
-			'se'=>'搜索引擎',
-			'keywords'=>'搜索关键词',
 		);
 	}
 
 	public function filters(){
 		return array(
+			'id'=>'intval',
 			'username'=>'trim',
 			'email'=>'trim',
-			'cellphone'=>'trim',
+			'mobile'=>'trim',
 			'password'=>'trim',
 			'salt'=>'trim',
-			'realname'=>'trim',
 			'nickname'=>'trim',
-			'professional'=>'trim',
-			'sex'=>'trim',
 			'avatar'=>'intval',
-			'reg_time'=>'trim',
-			'reg_ip'=>'intval',
-			'login_times'=>'trim',
-			'last_login_time'=>'trim',
-			'last_login_ip'=>'intval',
-			'last_time_online'=>'trim',
 			'status'=>'intval',
 			'block'=>'intval',
-			'role'=>'intval',
 			'parent'=>'intval',
 			'deleted'=>'intval',
-			'trackid'=>'trim',
-			'refer'=>'trim',
-			'se'=>'trim',
-			'keywords'=>'trim',
 		);
 	}
 }

e89a6a32fff7fb8a2f71cc0179c8e3948da46287
fayfox <admin@fayfox.com>
2015-09-10 22:40:23 +0800

    glhs

--- application/glhs/configs/main.php
+++ application/glhs/configs/main.php
@@ -12,7 +12,7 @@ return array(
 		'user'=>'root',							//用户名
 		'password'=>'',							//密码
 		'port'=>3306,							//端口
-		'dbname'=>'fayfox_glhs',					//数据库名
+		'dbname'=>'faycms_glhs',					//数据库名
 		'charset'=>'utf8',						//数据库编码方式
 		'table_prefix'=>'faycms_',				//数据库表前缀
 	),
@@ -29,5 +29,5 @@ return array(
 		'frontend'
 	),
 	
-	'debug'=>false,
+	'debug'=>$_SERVER['HTTP_HOST'] == '55.fayfox.com' || $_SERVER['HTTP_X_FORWARDED_HOST'] == '55.fayfox.com' ? true : false,
 );
--- application/glhs/data/custom.sql
+++ application/glhs/data/custom.sql
@@ -1,7 +1,7 @@
 -- 页面
 INSERT INTO `{{$prefix}}pages` (id, title, alias) VALUES (1, '关于我们', 'about');
 
---　基础分类
+-- 基础分类
 INSERT INTO `{{$prefix}}categories` (`id`, `title`, `alias`, `parent`, `is_nav`, `is_system`) VALUES ('1000', '师资力量', 'teacher', '1', '1', '1');
 INSERT INTO `{{$prefix}}categories` (`id`, `title`, `alias`, `parent`, `is_nav`, `is_system`) VALUES ('1001', '学生作品', 'works', '1', '1', '1');
 INSERT INTO `{{$prefix}}categories` (`id`, `title`, `alias`, `parent`, `is_nav`, `is_system`) VALUES ('1002', '招生简章', 'guide', '1', '1', '1');

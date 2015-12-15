302d7f524f1deba6fdc960d372723f1f789dcb34
Fayfox <fayfox@haimi.com>
2015-12-15 16:10:28 +0800

    测试服数据库参数

--- application/pharmrich/configs/main.php
+++ application/pharmrich/configs/main.php
@@ -9,8 +9,8 @@ return array(
 	 */
 	'db'=>array(
 		'host'=>'localhost',					//数据库服务器
-		'user'=>'root',							//用户名
-		'password'=>'',							//密码
+		'user'=>$_SERVER['HTTP_HOST'] == 'pharmrich.faycms.com' ? 'faycms' : 'root',//用户名
+		'password'=>$_SERVER['HTTP_HOST'] == 'pharmrich.faycms.com' ? 'jDoBjHwVq6q2hQVN' : '',//密码
 		'port'=>3306,							//端口
 		'dbname'=>'faycms_pharmrich',					//数据库名
 		'charset'=>'utf8mb4',						//数据库编码方式

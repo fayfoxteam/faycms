9e293c4d3a2fc32d7e0cbef2f2e376f8235b5073
fayfox <admin@fayfox.com>
2016-02-28 17:19:30 +0800

    新增fay\helper\StringHelper::guidv4()

--- vendor/fay/helpers/StringHelper.php
+++ vendor/fay/helpers/StringHelper.php
@@ -203,6 +203,31 @@ class StringHelper{
 		}
 	}
 	
+	/**
+	 * 生成一个uuid
+	 */
+	public static function guidv4(){
+		return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+		// 32 bits for "time_low"
+		mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+
+		// 16 bits for "time_mid"
+		mt_rand( 0, 0xffff ),
+
+		// 16 bits for "time_hi_and_version",
+		// four most significant bits holds version number 4
+		mt_rand( 0, 0x0fff ) | 0x4000,
+
+		// 16 bits, 8 bits for "clk_seq_hi_res",
+		// 8 bits for "clk_seq_low",
+		// two most significant bits holds zero and one for variant DCE1.1
+		mt_rand( 0, 0x3fff ) | 0x8000,
+
+		// 48 bits for "node"
+		mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+	);
+	}
+	
 	public static function removeXSS($val) {
 		// remove all non-printable characters. CR(0a) and LF(0b) and TAB(9) are allowed
 		// this prevents some character re-spacing such as <java\0script>

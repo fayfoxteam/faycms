a77a2188d76b4ed1708aff87f8563fda950d26d5
Fayfox <fayfox@haimi.com>
2016-03-23 13:50:18 +0800

    fay\helpers\Request::ip2int当传入非法ip时，返回false

--- vendor/fay/helpers/Request.php
+++ vendor/fay/helpers/Request.php
@@ -30,7 +30,7 @@ class Request{
 	 * @return int
 	 */
 	public static function ip2int($ip){
-		if(!$r = ip2long($ip)) return 0;
+		if(!$r = ip2long($ip)) return $r;
 		if($r > 2147483647)
 			$r -= 4294967296;
 		return $r;

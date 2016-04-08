53fea38340fff6c197b3ee577a02b10358f6b9a3
Fayfox <fayfox@haimi.com>
2016-04-08 16:16:31 +0800

    apidoc

--- application/apidoc/modules/frontend/views/api/item.php
+++ application/apidoc/modules/frontend/views/api/item.php
@@ -3,6 +3,7 @@ use fay\core\Loader;
 use fay\helpers\Html;
 use apidoc\models\tables\Inputs;
 use apidoc\models\tables\Apis;
+use apidoc\helpers\ApiHelper;
 ?>
 <?php if($api['api']['description']){?>
 <div class="panel panel-headerless">
@@ -12,6 +13,32 @@ use apidoc\models\tables\Apis;
 	?></div>
 </div>
 <?php }?>
+<div class="panel">
+	<div class="panel-header"><h2>请求说明</h2></div>
+	<div class="panel-body">
+		<div class="form-group">
+			<label class="col-2">HTTP请求方式</label>
+			<div class="col-10 pt7"><?php
+				$http_mothods_map = Apis::getHttpMethods();
+				echo strtoupper($http_mothods_map[$api['api']['http_method']]);
+			?></div>
+		</div>
+		<div class="form-group">
+			<label class="col-2">是否需要登录</label>
+			<div class="col-10 pt7"><?php
+				echo $api['api']['need_login'] ? '<span class="required">是</span>' : '否';
+			?></div>
+		</div>
+		<div class="form-group">
+			<label class="col-2">状态</label>
+			<div class="col-10 pt7"><?php echo ApiHelper::getStatus($api['api']['status'])?></div>
+		</div>
+		<div class="form-group">
+			<label class="col-2">自从</label>
+			<div class="col-10 pt7"><?php echo $api['api']['since']?></div>
+		</div>
+	</div>
+</div>
 <div class="panel">
 	<div class="panel-header"><h2>请求参数</h2></div>
 	<div class="panel-body">
@@ -41,19 +68,6 @@ use apidoc\models\tables\Apis;
 	</div>
 </div>
 <div class="panel">
-	<div class="panel-header"><h2>HTTP请求方式</h2></div>
-	<div class="panel-body"><?php
-		$http_mothods_map = Apis::getHttpMethods();
-		echo strtoupper($http_mothods_map[$api['api']['http_method']]);
-	?></div>
-</div>
-<div class="panel">
-	<div class="panel-header"><h2>是否需要登录</h2></div>
-	<div class="panel-body"><?php
-		echo $api['api']['need_login'] ? '<span class="required">是</span>' : '否';
-	?></div>
-</div>
-<div class="panel">
 	<div class="panel-header"><h2>响应参数</h2></div>
 	<div class="panel-body"><?php
 		
@@ -61,7 +75,13 @@ use apidoc\models\tables\Apis;
 </div>
 <div class="panel">
 	<div class="panel-header"><h2>响应示例</h2></div>
-	<div class="panel-body"><pre class="prettyprint json"><code><?php
-		echo $api['api']['sample_response'];
-	?></code></pre></div>
+	<div class="panel-body">
+	<?php if($api['api']['sample_response']){?>
+		<pre class="prettyprint json"><code><?php
+			echo $api['api']['sample_response'];
+		?></code></pre>
+	<?php }else{?>
+		<span>无</span>
+	<?php }?>
+	</div>
 </div>

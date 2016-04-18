ca4151a650437417c9dfa88aae17ce796222b704
Fayfox <fayfox@haimi.com>
2016-04-18 14:55:02 +0800

    tools/function/json

--- vendor/cms/modules/tools/views/function/json.php
+++ vendor/cms/modules/tools/views/function/json.php
@@ -42,12 +42,58 @@
 			</div>
 		</div>
 	</div>
+	<div class="row">
+		<div class="col-6">
+			<div class="box">
+				<div class="box-title"><h3>format</h3></div>
+				<div class="box-content">
+					<?php echo F::form()->textarea('format_json', array(
+						'class'=>'form-control h200 autosize',
+					));?>
+				</div>
+			</div>
+		</div>
+		<div class="col-6">
+			<div class="box">
+				<div class="box-title"><h3>Result</h3></div>
+				<div class="box-content">
+					<div style="min-height:239px"><textarea class="form-control h200 autosize" id="formatted-json"></textarea></div>
+				</div>
+			</div>
+		</div>
+	</div>
 </form>
 <script>
-$("[name='json'],[name='array']").keydown(function(event){
+$("[name='json'],[name='array']").on('keydown', function(event){
 	if((event.keyCode == 82 || event.keyCode == 83) && event.ctrlKey){
 		$("#form").submit();
 		return false;
+	}
+});
+$("[name='format_json']").on('keydown', function(event){
+	if((event.keyCode == 82 || event.keyCode == 83) && event.ctrlKey){
+		try{
+			var jsonObj = $.parseJSON($("[name='format_json']").val());
+		}catch(e){
+			jsonObj = false;
+		}
+		
+		if(jsonObj){
+			$("#formatted-json").val(JSON.stringify(jsonObj, null, 4));
+			autosize.update($("#formatted-json"));
+		}
+	}
+});
+$("[name='format_json']").on('blur', function(){
+	try{
+		var jsonObj = $.parseJSON($("[name='format_json']").val());
+	}catch(e){
+		jsonObj = false;
+	}
+	
+	if(jsonObj){
+		$("#formatted-json").val(JSON.stringify(jsonObj, null, 4));
+		autosize.update($("#formatted-json"));
 	}
 });
 </script>

c40df2dc23108479a51ad17187aabbb894f8f53f
fayfox <admin@fayfox.com>
2016-04-18 22:12:59 +0800

    tools/function/json::format换到最前面

--- vendor/cms/modules/tools/views/function/json.php
+++ vendor/cms/modules/tools/views/function/json.php
@@ -2,6 +2,26 @@
 	<div class="row">
 		<div class="col-6">
 			<div class="box">
+				<div class="box-title"><h3>format</h3></div>
+				<div class="box-content">
+					<?php echo F::form()->textarea('format_json', array(
+						'class'=>'form-control h200 autosize',
+					));?>
+				</div>
+			</div>
+		</div>
+		<div class="col-6">
+			<div class="box">
+				<div class="box-title"><h3>Result</h3></div>
+				<div class="box-content">
+					<div><textarea class="form-control h200 autosize" id="formatted-json"></textarea></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-6">
+			<div class="box">
 				<div class="box-title"><h3>json_decode</h3></div>
 				<div class="box-content">
 					<?php echo F::form()->textarea('json', array(
@@ -38,26 +58,6 @@
 				<div class="box-title"><h3>Result</h3></div>
 				<div class="box-content">
 					<div style="min-height:239px"><textarea class="form-control h200 autosize"><?php echo json_encode(eval('return '.F::input()->post('array').';'));?></textarea></div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-6">
-			<div class="box">
-				<div class="box-title"><h3>format</h3></div>
-				<div class="box-content">
-					<?php echo F::form()->textarea('format_json', array(
-						'class'=>'form-control h200 autosize',
-					));?>
-				</div>
-			</div>
-		</div>
-		<div class="col-6">
-			<div class="box">
-				<div class="box-title"><h3>Result</h3></div>
-				<div class="box-content">
-					<div style="min-height:239px"><textarea class="form-control h200 autosize" id="formatted-json"></textarea></div>
 				</div>
 			</div>
 		</div>

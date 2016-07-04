0da9e518c4af85a6e208538514d9e2be8aa7a624
fayfox <admin@fayfox.com>
2016-07-04 23:53:31 +0800

    church

---
+++ application/church/configs/main.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 该文件的配置项将覆盖外层/config/main.php的配置
+ * 若不设置则默认使用外面的配置参数
+ */
+return array(
+	/*
+	 * 数据库参数
+	 */
+	'db'=>array(
+		'host'=>'localhost',					//数据库服务器
+		'user'=>'root',							//用户名
+		'password'=>'',							//密码
+		'port'=>3307,							//端口
+		'dbname'=>'faycms_church',					//数据库名
+		'charset'=>'utf8',						//数据库编码方式
+		'table_prefix'=>'faycms_',				//数据库表前缀
+	),
+	
+	/*
+	 * 当前application包含的模块
+	 */
+	'modules'=>array(
+		'frontend'
+	),
+);

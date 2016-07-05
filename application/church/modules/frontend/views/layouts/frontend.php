a4e1ec52ca4fc9445b49a98c5dd218616b1c7de2
fayfox <admin@fayfox.com>
2016-07-05 23:39:19 +0800

    church

--- application/church/modules/frontend/views/layouts/frontend.php
+++ application/church/modules/frontend/views/layouts/frontend.php
@@ -15,6 +15,7 @@ use fay\helpers\Html;
 		echo Option::get('site:sitename')?></title>
 	<meta content="<?php if(isset($keywords))echo Html::encode($keywords);?>" name="keywords" />
 	<meta content="<?php if(isset($description))echo Html::encode($description);?>" name="description" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link type="text/css" rel="stylesheet" href="<?php echo $this->assets('faycms/css/frontend.css')?>" >
 	<link type="text/css" rel="stylesheet" href="<?php echo $this->appStatic('css/style.css')?>" >
 	<?php echo $this->getCss()?>
@@ -27,7 +28,7 @@ use fay\helpers\Html;
 </head>
 <body>
 <div class="wrapper">
-	<header>
+	<header class="page-header">
 		<nav class="top"></nav>
 		<nav class="main-menu">
 			<div class="container">
@@ -49,7 +50,7 @@ use fay\helpers\Html;
 	<div class="banner">
 		
 	</div>
-	<div class="container">
+	<div class="container page-content">
 		<div class="row">
 			<main class="col-md-8 post-list">
 				<article>
@@ -59,6 +60,7 @@ use fay\helpers\Html;
 					<div class="post-content">
 						<h2 class="post-title">带轮播图文章</h2>
 						<div class="post-meta">
+							<time class="post-meta-time">2天前</time>
 							<span class="post-meta-category">分类1</span>
 							<span class="post-meta-views">
 								<span>阅读数</span>
@@ -78,10 +80,72 @@ use fay\helpers\Html;
 				</div>
 				<div class="widget">
 					<h5 class="widget-title">小工具标题</h5>
+					<article>
+						<div class="post-thumb">
+							<a href=""><img src="http://55.fayfox.com/fayfox/file/pic/f/10000?t=4&dw=60&dh=60" /></a>
+						</div>
+						<div class="post-content">
+							<h5 class="post-title">这是一个文章标题</h5>
+							<div class="post-meta">
+								<span class="post-meta-category">分类1</span>
+								<time class="post-meta-time">3天前</time>
+							</div>
+						</div>
+					</article>
+					<article>
+						<div class="post-thumb">
+							<a href=""><img src="http://55.fayfox.com/fayfox/file/pic/f/10000?t=4&dw=60&dh=60" /></a>
+						</div>
+						<div class="post-content">
+							<h5 class="post-title">这是一个文章标题</h5>
+							<div class="post-meta">
+								<span class="post-meta-category">分类1</span>
+								<time class="post-meta-time">3天前</time>
+							</div>
+						</div>
+					</article>
 				</div>
 			</aside>
 		</div>
 	</div>
+	<footer class="page-footer">
+		<div class="container">
+			<div class="row">
+				<aside class="col-md-3">
+					<h4>关于我们</h4>
+					<div>
+						<p>We welcome visitors at NewLife Church and would love to have you join us in church this weekend.</p>
+						<p><b>E: info@yoursite.com</b><br>
+							<b>L: <a href="#">Google Map</a></b></p>
+					</div>
+				</aside>
+				<aside class="col-md-3">
+					<h4>关于我们</h4>
+					<div>
+						<p>We welcome visitors at NewLife Church and would love to have you join us in church this weekend.</p>
+						<p><b>E: info@yoursite.com</b><br>
+							<b>L: <a href="#">Google Map</a></b></p>
+					</div>
+				</aside>
+				<aside class="col-md-3">
+					<h4>关于我们</h4>
+					<div>
+						<p>We welcome visitors at NewLife Church and would love to have you join us in church this weekend.</p>
+						<p><b>E: info@yoursite.com</b><br>
+							<b>L: <a href="#">Google Map</a></b></p>
+					</div>
+				</aside>
+				<aside class="col-md-3">
+					<h4>关于我们</h4>
+					<div>
+						<p>We welcome visitors at NewLife Church and would love to have you join us in church this weekend.</p>
+						<p><b>E: info@yoursite.com</b><br>
+							<b>L: <a href="#">Google Map</a></b></p>
+					</div>
+				</aside>
+			</div>
+		</div>
+	</footer>
 </div>
 </body>
 </html>

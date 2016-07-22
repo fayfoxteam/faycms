19964abda8e742bb2db5faa89190b51f1946d452
fayfox <admin@fayfox.com>
2016-07-22 20:38:59 +0800

    blog

--- application/blog/configs/main.php
+++ application/blog/configs/main.php
@@ -11,7 +11,7 @@ return array(
 		'host'=>'localhost',					//数据库服务器
 		'user'=>$_SERVER['HTTP_HOST'] == 'blog.faycms.com' ? 'faycms' : 'root',//用户名
 		'password'=>$_SERVER['HTTP_HOST'] == 'blog.faycms.com' ? 'jDoBjHwVq6q2hQVN' : '',//密码
-		'port'=>3307,							//端口
+		'port'=>$_SERVER['HTTP_HOST'] == 'blog.faycms.com' ? 3306 : 3307,							//端口
 		'dbname'=>'faycms_blog',				//数据库名
 		'charset'=>'utf8',						//数据库编码方式
 		'table_prefix'=>'fayfox_',				//数据库表前缀

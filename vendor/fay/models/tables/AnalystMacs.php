49d6e8e56dfc0a74fae628730ec68500d5babfad
Fayfox <fayfox@haimi.com>
2016-07-27 16:29:19 +0800

    fay\models\table\AnalystMacs模型字段修改

--- vendor/fay/models/tables/AnalystMacs.php
+++ vendor/fay/models/tables/AnalystMacs.php
@@ -3,6 +3,30 @@ namespace fay\models\tables;
 
 use fay\core\db\Table;
 
+/**
+ * Analyst Macs model
+ *
+ * @property int $id Id
+ * @property string $user_agent User Agent
+ * @property string $browser 浏览器内核
+ * @property string $browser_version 内核版本
+ * @property string $shell 浏览器套壳
+ * @property string $shell_version 套壳版本
+ * @property string $os 操作系统
+ * @property int $ip_int Ip Int
+ * @property int $screen_width 屏幕宽度
+ * @property int $screen_height 屏幕高度
+ * @property string $url Url
+ * @property string $refer 来源url
+ * @property string $se Se
+ * @property string $keywords Keywords
+ * @property string $fmac FMac
+ * @property int $create_time 创建时间
+ * @property string $create_date 创建日期
+ * @property int $hour Hour
+ * @property string $trackid Trackid
+ * @property int $site Site
+ */
 class AnalystMacs extends Table{
 	protected $_name = 'analyst_macs';
 	
@@ -24,7 +48,7 @@ class AnalystMacs extends Table{
 			array(array('browser', 'browser_version', 'shell', 'shell_version', 'os', 'trackid'), 'string', array('max'=>30)),
 			array(array('se'), 'string', array('max'=>10)),
 			array(array('keywords'), 'string', array('max'=>50)),
-			array(array('hash'), 'string', array('max'=>32)),
+			array(array('fmac'), 'string', array('max'=>36)),
 		);
 	}
 
@@ -44,7 +68,7 @@ class AnalystMacs extends Table{
 			'refer'=>'来源url',
 			'se'=>'Se',
 			'keywords'=>'Keywords',
-			'hash'=>'Hash',
+			'fmac'=>'FMac',
 			'create_time'=>'创建时间',
 			'create_date'=>'创建日期',
 			'hour'=>'Hour',
@@ -67,7 +91,7 @@ class AnalystMacs extends Table{
 			'refer'=>'trim',
 			'se'=>'trim',
 			'keywords'=>'trim',
-			'hash'=>'trim',
+			'fmac'=>'trim',
 			'create_date'=>'',
 			'hour'=>'intval',
 			'trackid'=>'trim',

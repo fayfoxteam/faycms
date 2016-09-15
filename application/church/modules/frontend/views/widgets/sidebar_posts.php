dab7af25393c25dbbac11d8bcfb6df0bccb2b300
fayfox <admin@fayfox.com>
2016-09-15 23:03:42 +0800

    church

--- application/church/modules/frontend/views/widgets/sidebar_posts.php
+++ application/church/modules/frontend/views/widgets/sidebar_posts.php
@@ -5,7 +5,6 @@ use fay\helpers\Html;
  * @var array $posts
  */
 ?>
-<?php dump($posts)?>
 <div class="widget">
 	<h5 class="widget-title"><?php echo Html::encode($config['title'])?></h5>
 	<?php foreach($posts as $p){?>
@@ -20,24 +19,12 @@ use fay\helpers\Html;
 					echo Html::link($p['post']['title'], $p['post']['link'])
 				?></h5>
 				<div class="post-meta">
-					<span class="post-meta-category">分类1</span>
+					<?php echo Html::link($p['category']['title'], array('cat/'.$p['category']['alias']), array(
+						'class'=>'post-meta-category',
+					))?>
 					<time class="post-meta-time"><?php echo $p['post']['format_publish_time']?></time>
 				</div>
 			</div>
 		</article>
 	<?php }?>
-	<article>
-		<div class="post-thumb">
-			<a href=""><img src="http://55.fayfox.com/fayfox/file/pic/f/10000?t=4&dw=60&dh=60" /></a>
-		</div>
-		<div class="post-container">
-			<h5 class="post-title">
-				<a href="">这是一个文章标题</a>
-			</h5>
-			<div class="post-meta">
-				<span class="post-meta-category">分类1</span>
-				<time class="post-meta-time">3天前</time>
-			</div>
-		</div>
-	</article>
 </div>

40137ccb098d479fc674751a38604da4e49970a8
fayfox <admin@fayfox.com>
2016-11-12 18:39:26 +0800

    创建app bug修复

--- vendor/faysoft/cms/modules/tools/views/application/create.php
+++ vendor/faysoft/cms/modules/tools/views/application/create.php
@@ -86,7 +86,7 @@ use fay\helpers\Html;
 				<div id="install-db">
 					<div class="form-field">
 						<label class="title bold">站点名称<em class="fc-red">*</em></label>
-						<?php echo Html::inputText('site:sitename', '', array(
+						<?php echo Html::inputText('sitename', '', array(
 							'data-required'=>'required',
 							'data-label'=>'站点名称',
 							'class'=>'form-control mw400',

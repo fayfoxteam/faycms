c650e61e75a114ba72050dde0b46ae021351730e
fayfox <admin@fayfox.com>
2016-11-28 22:54:51 +0800

    fay\widgets\widgetarea

---
+++ vendor/faysoft/fay/widgets/widgetarea/controllers/AdminController.php
@@ -0,0 +1,46 @@
+<?php
+namespace fay\widgets\widgetarea\controllers;
+
+use fay\helpers\ArrayHelper;
+use fay\widget\Widget;
+use fay\services\Flash;
+
+class AdminController extends Widget{
+	public function index($config){
+		//获取默认模版
+		if(empty($config['template'])){
+			$config['template'] = file_get_contents(__DIR__.'/../views/index/template.php');
+			$this->form->setData(array(
+				'template'=>$config['template'],
+			), true);
+		}
+		
+		$this->view->assign(array(
+			'widgetareas'=>ArrayHelper::column(\F::config()->getFile('widgetareas'), 'description', 'alias'),
+			'config'=>$config
+		))->render();
+	}
+	
+	public function onPost(){
+		$this->setConfig($this->form->getFilteredData());
+		Flash::set('编辑成功', 'success');
+	}
+	
+	public function rules(){
+		return array(
+			array(array('alias'), 'required'),
+		);
+	}
+	
+	public function labels(){
+		return array(
+			'alias'=>'小工具域',
+		);
+	}
+	
+	public function filters(){
+		return array(
+			'alias'=>'trim',
+		);
+	}
+}
---
+++ vendor/faysoft/fay/widgets/widgetarea/views/index/template.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * @var $config
+ */
+F::widget()->area($config['alias']);

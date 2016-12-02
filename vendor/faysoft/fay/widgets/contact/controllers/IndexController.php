430f8b4e9f8f255e311d9c45cce5d6deb2da8640
Fayfox <fayfox@haimi.com>
2016-12-02 10:29:10 +0800

    fay\widgets\contact

--- vendor/faysoft/fay/widgets/contact/controllers/IndexController.php
+++ vendor/faysoft/fay/widgets/contact/controllers/IndexController.php
@@ -9,8 +9,15 @@ class IndexController extends Widget{
 	}
 	
 	public function index($config){
-		if(!$config['alias']){
-			return '';
+		$elements = array();
+		if(!empty($config['elements'])){
+			foreach($config['elements'] as $element){
+				$elements[] = array(
+					'name'=>$element,
+					'label'=>isset($config['labels'][$element]) ? $config['labels'][$element] : '',
+					'placeholder'=>isset($config['placeholder'][$element]) ? $config['placeholder'][$element] : '',
+				);
+			}
 		}
 		
 		//template
@@ -18,12 +25,14 @@ class IndexController extends Widget{
 			$this->view->render('template', array(
 				'config'=>$config,
 				'alias'=>$this->alias,
+				'elements'=>$elements,
 			));
 		}else{
 			if(preg_match('/^[\w_-]+(\/[\w_-]+)+$/', $config['template'])){
 				\F::app()->view->renderPartial($config['template'], array(
 					'config'=>$config,
 					'alias'=>$this->alias,
+					'elements'=>$elements,
 				));
 			}else{
 				$alias = $this->alias;

e3c8081ede72e9a044a0fda8150c860234529093
fayfox <admin@fayfox.com>
2017-01-11 20:27:02 +0800

    手机加速度测试demo

--- application/blog/modules/frontend/views/test/devicemotion.php
+++ application/blog/modules/frontend/views/test/devicemotion.php
@@ -2,12 +2,14 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script type="text/javascript" src="<?php echo $this->assets('js/jquery-1.7.1.min.js')?>"></script>
 </head>
 <body>
 <p><label>x轴加速度：</label><span id="acceleration-x"></span></p>
 <p><label>y轴加速度：</label><span id="acceleration-y"></span></p>
 <p><label>z轴加速度：</label><span id="acceleration-z"></span></p>
+<p><label>速度：</label><span id="speed"></span></p>
 <script>
 //先判断设备是否支持HTML5摇一摇功能
 if (window.DeviceMotionEvent) {
@@ -17,6 +19,8 @@ if (window.DeviceMotionEvent) {
 	alert('您好，你目前所用的设备好像不支持重力感应哦！');
 }
 
+var x, y, z, last_x, last_y, last_z;
+var last_update = 0;
 function deviceMotionHandler(event){
 	//获得重力加速
 	var acceleration =event.accelerationIncludingGravity;
@@ -24,6 +28,19 @@ function deviceMotionHandler(event){
 	$('#acceleration-x').text(acceleration.x);
 	$('#acceleration-y').text(acceleration.y);
 	$('#acceleration-z').text(acceleration.z);
+	
+	var diffTime = curTime - last_update;
+	last_update = curTime;
+	x = acceleration.x;
+	y = acceleration.y;
+	z = acceleration.z;
+	
+	var speed = Math.abs(x +y + z - last_x - last_y - last_z) / diffTime * 10000;
+	$('#speed').text(speed);
+	last_x = x;
+	last_y = y;
+	last_z = z;
+	
 }
 </script>
 </body>

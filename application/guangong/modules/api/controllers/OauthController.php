2c06adbd8d125fa18933cbcb3492b2c7b7529d2b
fayfox <admin@fayfox.com>
2017-01-15 21:16:33 +0800

    关公点兵

--- application/guangong/modules/api/controllers/OauthController.php
+++ application/guangong/modules/api/controllers/OauthController.php
@@ -2,7 +2,18 @@
 namespace guangong\modules\api\controllers;
 
 use cms\library\ApiController;
+use fay\helpers\UrlHelper;
+use fay\services\oauth\weixin\WeixinClient;
 
 class OauthController extends ApiController{
+	public function weixinGetCode(){
+		$client = new WeixinClient('wxad76a044d8fad0ed', '88efdec5df431446c3c42a8ee4004b9d');
+		$client->setRedirectUri(UrlHelper::createUrl('api/oauth/weixin-user-info'));
+		
+		header('location:' . $client->getAuthorizeUrl());
+	}
 	
+	public function weixinUserInfo(){
+		
+	}
 }

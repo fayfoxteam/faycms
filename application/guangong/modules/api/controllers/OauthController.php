7c61d88700fd2d4a263322cbb8190d5628fad1b7
fayfox <admin@fayfox.com>
2017-01-15 22:15:18 +0800

    关公点兵：微信登录测试

--- application/guangong/modules/api/controllers/OauthController.php
+++ application/guangong/modules/api/controllers/OauthController.php
@@ -2,21 +2,23 @@
 namespace guangong\modules\api\controllers;
 
 use cms\library\ApiController;
-use fay\helpers\UrlHelper;
 use fay\services\oauth\weixin\WeixinClient;
 
 class OauthController extends ApiController{
-	public function weixinGetCode(){
-		$client = new WeixinClient('wxad76a044d8fad0ed', '88efdec5df431446c3c42a8ee4004b9d');
-		$client->setRedirectUri(UrlHelper::createUrl('api/oauth/weixin-user-info'));
-		$client->setScope('snsapi_userinfo');//需要获取用户信息
+	public function weixin(){
+		$app_id = 'wxad76a044d8fad0ed';
+		$app_secret = '88efdec5df431446c3c42a8ee4004b9d';
 		
-		header('location:' . $client->getAuthorizeUrl());
-	}
-	
-	public function weixinUserInfo(){
-		$client = new WeixinClient('wxad76a044d8fad0ed', '88efdec5df431446c3c42a8ee4004b9d');
-		$access_token = $client->getAccessToken($this->input->get('code'));
+		$client = new WeixinClient($app_id, $app_secret);
+		if(!$code = $this->input->get('code')){
+			//跳转到微信拉去授权
+			$client->setScope('snsapi_userinfo');//需要获取用户信息（默认为：snsapi_base）
+			
+			header('location:' . $client->getAuthorizeUrl());
+		}
+		
+		$access_token = $client->getAccessToken($code);
+		
 		dump($access_token->getUser());
 	}
 }

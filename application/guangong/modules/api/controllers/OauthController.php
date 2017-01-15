454f9e583d59ed24aaee17c3b9aa25e244a0478c
fayfox <admin@fayfox.com>
2017-01-15 22:08:09 +0800

    关公点兵：微信登录测试

--- application/guangong/modules/api/controllers/OauthController.php
+++ application/guangong/modules/api/controllers/OauthController.php
@@ -9,6 +9,7 @@ class OauthController extends ApiController{
 	public function weixinGetCode(){
 		$client = new WeixinClient('wxad76a044d8fad0ed', '88efdec5df431446c3c42a8ee4004b9d');
 		$client->setRedirectUri(UrlHelper::createUrl('api/oauth/weixin-user-info'));
+		$client->setScope('snsapi_userinfo');//需要获取用户信息
 		
 		header('location:' . $client->getAuthorizeUrl());
 	}

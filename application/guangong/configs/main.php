6f8118765eeb6714ec902c559485a9fdfee8bc4b
fayfox <admin@fayfox.com>
2017-01-15 21:09:38 +0800

    关公点兵

--- application/guangong/configs/main.php
+++ application/guangong/configs/main.php
@@ -9,11 +9,11 @@ return array(
 	 */
 	'db'=>array(
 		'host'=>'localhost',					//数据库服务器
-		'user'=>'root',							//用户名
-		'password'=>'',							//密码
+		'user'=>'faycms',							//用户名
+		'password'=>'jDoBjHwVq6q2hQVN',							//密码
 		'port'=>3306,							//端口
 		'dbname'=>'faycms_guangong',					//数据库名
-		'charset'=>'utf8mb4',						//数据库编码方式
+		'charset'=>preg_match('/^(\d+).fayfox.com$/', isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : $_SERVER['HTTP_HOST']) ? 'utf8mb4' : 'utf8',						//数据库编码方式
 		'table_prefix'=>'faycms_',				//数据库表前缀
 	),
 	

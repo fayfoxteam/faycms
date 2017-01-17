a182a6c61574411ec5a5bc528012071545be3093
Fayfox <fayfox@haimi.com>
2017-01-17 17:22:18 +0800

    关公点兵：支付测试

---
+++ application/guangong/modules/api/controllers/PaymentController.php
@@ -0,0 +1,22 @@
+<?php
+namespace guangong\modules\api\controllers;
+
+use guangong\library\FrontController;
+use fay\helpers\UrlHelper;
+use fay\payments\PaymentConfig;
+use fay\payments\PaymentTrade;
+use fay\payments\weixin\WeixinPayment;
+
+class PaymentController extends FrontController{
+	public function wxjsapi(){
+		$trade = new PaymentTrade('fayfox'.date("YmdHis"), 1);
+		$trade->setNotifyUrl(UrlHelper::createUrl('payment/wx-notify'))
+			->setBody('fayfox测试订单');
+		
+		$config = new PaymentConfig('123');
+		$config->setAppId('wxad76a044d8fad0ed');
+		
+		$payment = new WeixinPayment();
+		$payment->jsApi($trade, $config);
+	}
+}

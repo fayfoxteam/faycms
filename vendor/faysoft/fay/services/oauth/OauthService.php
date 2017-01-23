06d4a1e6ff745509706a13030279a8296b9917cb
fayfox <admin@fayfox.com>
2017-01-23 14:16:28 +0800

    OAuth代码格式优化

--- vendor/faysoft/fay/services/oauth/OauthService.php
+++ vendor/faysoft/fay/services/oauth/OauthService.php
@@ -7,38 +7,50 @@ use fay\services\oauth\weixin\WeixinClient;
 use fay\services\OptionService;
 
 class OauthService extends Service{
-    /**
-     * @param string $class_name
-     * @return OauthService
-     */
-    public static function service($class_name = __CLASS__){
-        return parent::service($class_name);
-    }
+	/**
+	 * @param string $class_name
+	 * @return OauthService
+	 */
+	public static function service($class_name = __CLASS__){
+		return parent::service($class_name);
+	}
+	
+	/**
+	 * 获取微信Access Token
+	 * @return \fay\services\oauth\weixin\WeixinAccessToken
+	 * @throws OAuthException
+	 */
+	public function getWeixinAccessToken(){
+		$config = $this->getConfig('oauth:weixin');
+	
+		$client = new WeixinClient($config['app_id'], $config['app_secret']);
+		if(!$code = \F::input()->get('code')){
+			//需要获取用户信息（默认为：snsapi_base）
+			$client->setScope('snsapi_userinfo');
+		
+			//跳转到微信拉取授权
+			Response::redirect($client->getAuthorizeUrl());
+		}
+	
+		return $client->getAccessToken($code);
+	}
     
     /**
-     * 获取微信Access Token
-     * @return \fay\services\oauth\weixin\WeixinAccessToken
+     * 获取指定OAuth配置参数
+     * @param string $key
+     * @return array
      * @throws OAuthException
      */
-    public function getWeixinAccessToken(){
-        $config = OptionService::getGroup('oauth:weixin');
-        if(!$config){
-            throw new OAuthException('微信登录参数未设置');
-        }
+	private function getConfig($key){
+		$config = OptionService::getGroup($key);
+		if(!$config){
+			throw new OAuthException("{{$key}}Oauth参数未设置");
+		}
+	
+		if(empty($config['enabled'])){
+			throw new OAuthException("{{$key}}Oauth登录已禁用");
+		}
         
-        if(empty($config['enabled'])){
-            throw new OAuthException('微信登录已禁用');
-        }
-    
-        $client = new WeixinClient($config['app_id'], $config['app_secret']);
-        if(!$code = \F::input()->get('code')){
-            //需要获取用户信息（默认为：snsapi_base）
-            $client->setScope('snsapi_userinfo');
-        
-            //跳转到微信拉取授权
-            Response::redirect($client->getAuthorizeUrl());
-        }
-    
-        return $client->getAccessToken($code);
-    }
+        return $config;
+	}
 }

ba7caca9d9891929ba61180f379104865e256fcc
fayfox <admin@fayfox.com>
2017-01-28 14:32:37 +0800

    关公点兵

---
+++ application/guangong/modules/frontend/controllers/RecruitController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace guangong\modules\frontend\controllers;
+
+use guangong\library\FrontController;
+
+/**
+ * 天下招募令
+ */
+class RecruitController extends FrontController{
+	public function index(){
+		$this->view->render();
+	}
+}
---
+++ application/guangong/modules/frontend/views/recruit/index.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @var $this \fay\core\View
+ */
+$this->appendCss($this->appStatic('css/recruit.css'));
+?>
+<div class="swiper-container groups">
+	<div class="swiper-wrapper">
+		<div class="swiper-slide" id="recruit-1"></div>
+		<div class="swiper-slide" id="recruit-2">
+			<div class="layer brand"><img src="<?php echo $this->appStatic('images/recruit/brand.png')?>"></div>
+			<div class="layer text"><img src="<?php echo $this->appStatic('images/recruit/2.png')?>"></div>
+		</div>
+		<div class="swiper-slide" id="recruit-3">
+			<div class="layer brand"><img src="<?php echo $this->appStatic('images/recruit/brand.png')?>"></div>
+			<div class="layer steps">
+				<a href="javascript:;" data-slide="3" class="swiper-to"><img src="<?php echo $this->appStatic('images/recruit/s5.png')?>"></a>
+				<a href="javascript:;" data-slide="4" class="swiper-to"><img src="<?php echo $this->appStatic('images/recruit/s4.png')?>"></a>
+				<a href="javascript:;" data-slide="8" class="swiper-to"><img src="<?php echo $this->appStatic('images/recruit/s3.png')?>"></a>
+				<a href="javascript:;" data-slide="6" class="swiper-to"><img src="<?php echo $this->appStatic('images/recruit/s2.png')?>"></a>
+				<a href="javascript:;" data-slide="3" class="swiper-to"><img src="<?php echo $this->appStatic('images/recruit/s1.png')?>"></a>
+			</div>
+		</div>
+		<div class="swiper-slide" id="recruit-4">
+			<div class="layer brand"><img src="<?php echo $this->appStatic('images/recruit/brand.png')?>"></div>
+			<div class="layer dadao"><img src="<?php echo $this->appStatic('images/recruit/dadao.png')?>"></div>
+			<div class="layer title"><img src="<?php echo $this->appStatic('images/recruit/t1.png')?>"></div>
+			<div class="layer description">
+				<p>体验规则：</p>
+				<p>凡入关羽军团者，请详读招募令并领会其意图。</p>
+			</div>
+		</div>
+		<div class="swiper-slide" id="recruit-5">
+			<div class="layer brand"><img src="<?php echo $this->appStatic('images/recruit/brand.png')?>"></div>
+			<div class="layer zhaomuling"><img src="<?php echo $this->appStatic('images/recruit/zhaomuling.png')?>"></div>
+			<div class="layer zhaomuling-text"><img src="<?php echo $this->appStatic('images/recruit/zhaomuling-text.png')?>"></div>
+		</div>
+		<div class="swiper-slide" id="recruit-6">
+			<div class="layer yi"><img src="<?php echo $this->appStatic('images/recruit/yi.png')?>"></div>
+			<div class="layer yi-text"><img src="<?php echo $this->appStatic('images/recruit/yi-text.png')?>"></div>
+		</div>
+		<div class="swiper-slide" id="recruit-7">
+			<div class="layer brand"><img src="<?php echo $this->appStatic('images/recruit/brand.png')?>"></div>
+			<div class="layer dadao"><img src="<?php echo $this->appStatic('images/recruit/dadao.png')?>"></div>
+			<div class="layer title"><img src="<?php echo $this->appStatic('images/recruit/t2.png')?>"></div>
+			<div class="layer description">
+				<p>拜祭规则：</p>
+				<p>崇信关公忠义精神之三国迷、军事迷、谋略迷及正直青年，诚心加入关羽军团者，请拜关将军。无心者，请止步。</p>
+			</div>
+		</div>
+		<div class="swiper-slide" id="recruit-8">
+			<div class="layer brand"><img src="<?php echo $this->appStatic('images/recruit/brand.png')?>"></div>
+			<div class="layer guangong"><img src="<?php echo $this->appStatic('images/group/guangong.png')?>"></div>
+			<div class="layer steps">
+				<a href="javascript:;"><img src="<?php echo $this->appStatic('images/recruit/2-3.png')?>"></a>
+				<a href="javascript:;"><img src="<?php echo $this->appStatic('images/recruit/2-2.png')?>"></a>
+				<a href="javascript:;"><img src="<?php echo $this->appStatic('images/recruit/2-1.png')?>"></a>
+			</div>
+			<div class="layer jiangjunshengping">
+				<a href="javascript:;"><img src="<?php echo $this->appStatic('images/recruit/jiangjunshengping.png')?>"></a>
+			</div>
+		</div>
+		<div class="swiper-slide" id="recruit-9">
+			<div class="layer brand"><img src="<?php echo $this->appStatic('images/recruit/brand.png')?>"></div>
+			<div class="layer dadao"><img src="<?php echo $this->appStatic('images/recruit/dadao.png')?>"></div>
+			<div class="layer title"><img src="<?php echo $this->appStatic('images/recruit/t3.png')?>"></div>
+			<div class="layer description">
+				<p>应征规则：</p>
+				<p>加入关于军团须提交相关个人信息纳入网络军籍存档，请保证档案信息真实性和严肃性；同时分担个位数小额军费。本系统按要求确保个人信息安全。</p>
+			</div>
+		</div>
+	</div>
+</div>

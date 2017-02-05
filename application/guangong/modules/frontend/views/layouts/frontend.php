832343ce6e106139814ba03de94b08e69db4050f
Fayfox <fayfox@haimi.com>
2017-02-05 17:27:15 +0800

    关公点兵

--- application/guangong/modules/frontend/views/layouts/frontend.php
+++ application/guangong/modules/frontend/views/layouts/frontend.php
@@ -20,6 +20,7 @@ echo OptionService::get('site:sitename')?></title>
 <?php echo $this->getCss()?>
 <script type="text/javascript" src="<?php echo $this->assets('js/jquery-1.8.3.min.js')?>"></script>
 <script type="text/javascript" src="<?php echo $this->assets('faycms/js/system.min.js')?>"></script>
+<script type="text/javascript" src="<?php echo $this->appAssets('js/common.js')?>"></script>
 <script>
 system.base_url = '<?php echo $this->url()?>';
 system.user_id = '<?php echo \F::app()->current_user?>';
@@ -28,7 +29,6 @@ system.user_id = '<?php echo \F::app()->current_user?>';
 <body>
 <?php echo $content?>
 <script type="text/javascript" src="<?php echo $this->assets('faycms/js/fayfox.block.js')?>"></script>
-<script type="text/javascript" src="<?php echo $this->appAssets('js/common.js')?>"></script>
 <script>
 $(function(){
 	common.form.labels = <?php echo json_encode(F::form()->getLabels())?>;

fa7f08a39c19071f07155f7b1f463301fd3c3dc0
Fayfox <fayfox@haimi.com>
2017-02-06 18:11:02 +0800

    关公点兵：动效

--- application/guangong/modules/frontend/views/arm/set_arm.php
+++ application/guangong/modules/frontend/views/arm/set_arm.php
@@ -26,6 +26,13 @@ $this->appendCss($this->appAssets('css/arm.css'));
 			<?php }else{?>
 				<div class="layer arms"><img src="<?php echo $this->appAssets('images/arm/arms.png')?>"></div>
 				<div class="layer shake"><img src="<?php echo $this->appAssets('images/arm/shake.png')?>"></div>
+				<div class="arm-names">
+					<div class="layer bubing-text"><img src="<?php echo $this->appAssets('images/arm/bubing-text.png')?>"></div>
+					<div class="layer shuijun-text"><img src="<?php echo $this->appAssets('images/arm/shuijun-text.png')?>"></div>
+					<div class="layer qibing-text"><img src="<?php echo $this->appAssets('images/arm/qibing-text.png')?>"></div>
+					<div class="layer nubing-text"><img src="<?php echo $this->appAssets('images/arm/nubing-text.png')?>"></div>
+					<div class="layer chebing-text"><img src="<?php echo $this->appAssets('images/arm/chebing-text.png')?>"></div>
+				</div>
 			<?php }?>
 			<div class="layer arm-text"><img src="<?php echo $this->appAssets('images/arm/arm-text.png')?>"></div>
 			<div class="layer description">
@@ -36,6 +43,23 @@ $this->appendCss($this->appAssets('css/arm.css'));
 		<?php $this->renderPartial('_steps')?>
 	</div>
 </div>
+<?php $this->renderPartial('_js')?>
+<?php if(!$arm){?>
+<script src="<?php echo $this->assets('faycms/js/faycms.shake.js')?>"></script>
+<script>
+	$.shake(function(){
+		//摇一摇触发排勤务
+		if(common.swiper.activeIndex == 2){
+			arm.setArm();
+		}
+	});
+	$('.shake').on('click', function(){
+		arm.setArm();
+	})
+</script>
+<?php }else{?>
+<link type="text/css" rel="stylesheet" href="<?php echo $this->assets('css/jquery.fancybox-1.3.4.css')?>">
+<script type="text/javascript" src="<?php echo $this->assets('js/jquery.fancybox-1.3.4.pack.js')?>"></script>
 <div class="hide">
 	<div id="arm-dialog" class="dialog">
 		<div class="dialog-content">
@@ -43,35 +67,6 @@ $this->appendCss($this->appAssets('css/arm.css'));
 		</div>
 	</div>
 </div>
-<?php $this->renderPartial('_js')?>
-<link type="text/css" rel="stylesheet" href="<?php echo $this->assets('css/jquery.fancybox-1.3.4.css')?>">
-<script type="text/javascript" src="<?php echo $this->assets('js/jquery.fancybox-1.3.4.pack.js')?>"></script>
-<?php if(!$arm){?>
-<script src="<?php echo $this->assets('faycms/js/faycms.shake.js')?>"></script>
-<script>
-	$.shake(function(){
-		//摇一摇触发排勤务
-		if(common.swiper.activeIndex == 2){
-			$.ajax({
-				'type': 'GET',
-				'url': system.url('api/arm/set'),
-				'dataType': 'json',
-				'cache': false,
-				'success': function(resp){
-					if(resp.status){
-						var $arm6 = $('#arm-6');
-						$arm6.find('.arms').remove();
-						$arm6.find('.shake').remove();
-						$arm6.append('<div class="layer result"><img src="'+resp.data.picture.url+'"></div>');
-					}else{
-						common.toast($resp.message, 'error');
-					}
-				}
-			});
-		}
-	});
-</script>
-<?php }?>
 <script>
 	$('#arm-6 .result').fancybox({
 		'type': 'inline',
@@ -81,3 +76,4 @@ $this->appendCss($this->appAssets('css/arm.css'));
 		'width': '80%'
 	});
 </script>
+<?php }?>
--- public/apps/guangong/js/arm.js
+++ public/apps/guangong/js/arm.js
@@ -25,7 +25,7 @@ var arm = {
 		}else{
 			$('.defence-text').hide().removeClass('fadeInDown animated');
 		}
-		
+
 		//履军职任务列表
 		if($swiper.find('.jobs').length){
 			setTimeout(function(){
@@ -44,8 +44,53 @@ var arm = {
 			$('.jobs li').css({'visibility': 'hidden'}).removeClass('fadeIn animated');
 		}
 	},
+	/**
+	 * 循环动画，执行一次就好了，不用重复执行
+	 */
+	'interval': function(){
+		//兵种文字
+		if($('.arm-names').length){
+			setInterval(function(){
+				var $armNames = $('.arm-names');
+				$armNames.find('.layer').removeClass('tada animated');
+				//随机出一个0-5的整数
+				var num = Math.floor(Math.random()*6);
+				$armNames.find('.layer:eq('+num+')').addClass('tada animated');
+			}, 1000);
+		}
+
+		//摇一摇标识
+		if($('.shake').length){
+			setInterval(function(){
+				var $shake = $('.shake');
+				if($shake.hasClass('animated')){
+					$shake.removeClass('tada animated');
+				}else{
+					$shake.addClass('tada animated')
+				}
+			}, 1000);
+		}
+	},
+	'setArm': function(){
+		$.ajax({
+			'type': 'GET',
+			'url': system.url('api/arm/set'),
+			'dataType': 'json',
+			'cache': false,
+			'success': function(resp){
+				if(resp.status){
+					var $arm6 = $('#arm-6');
+					$arm6.find('.arms,.shake,.arm-names').remove();
+					$arm6.append('<div class="layer result flip animated"><img src="'+resp.data.picture.url+'"></div>');
+				}else{
+					common.toast($resp.message, 'error');
+				}
+			}
+		});
+	},
 	'init': function(){
 		this.animate();
+		this.interval();
 		common.swiper.on('SlideChangeStart', this.animate)
 	}
 };

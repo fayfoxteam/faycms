5baa189c25ba41fd8e26d26c88c9848f40af195e
fayfox <admin@fayfox.com>
2017-02-11 16:13:21 +0800

    情人节活动

--- application/valentine/modules/frontend/views/index/index.php
+++ application/valentine/modules/frontend/views/index/index.php
@@ -38,7 +38,7 @@
 <script>
 wx.config({
 	debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-	appId: '<?php echo $signature['app_id']?>', // 必填，公众号的唯一标识
+	appId: '<?php echo $signature['appId']?>', // 必填，公众号的唯一标识
 	timestamp: <?php echo $signature['timestamp']?>, // 必填，生成签名的时间戳
 	nonceStr: '<?php echo $signature['nonceStr']?>', // 必填，生成签名的随机串
 	signature: '<?php echo $signature['signature']?>',// 必填，签名，见附录1

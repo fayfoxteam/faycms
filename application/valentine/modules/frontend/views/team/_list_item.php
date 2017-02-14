858236049086fcc19c1d22af4f792867c9e1836a
fayfox <admin@fayfox.com>
2017-02-14 22:52:53 +0800

    图片使用原图

--- application/valentine/modules/frontend/views/team/_list_item.php
+++ application/valentine/modules/frontend/views/team/_list_item.php
@@ -18,9 +18,7 @@
 	?>" data-lightbox="teams"><?php
 		if($data['photo']){
 			//已经下载到本地，从本地输出
-			echo \fay\helpers\HtmlHelper::img($data['photo'], \fay\services\FileService::PIC_RESIZE, array(
-				'dw'=>400,
-			));
+			echo \fay\helpers\HtmlHelper::img($data['photo'], \fay\services\FileService::PIC_ORIGINAL);
 		}else{
 			//还在微信服务器，通过媒体ID输出
 			echo \fay\helpers\HtmlHelper::img("http://file.api.weixin.qq.com/cgi-bin/media/get?access_token={$access_token}&media_id={$data['photo_server_id']}");

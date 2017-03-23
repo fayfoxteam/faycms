9c4720dbddc7cee8581a410f2a69502a55e5ec19
fayfox <admin@fayfox.com>
2017-03-23 23:12:00 +0800

    关公点兵

--- application/guangong/modules/frontend/views/arm/_job_slides.php
+++ application/guangong/modules/frontend/views/arm/_job_slides.php
@@ -27,7 +27,81 @@
 			</li>
 		</ul>
 	</div>
-	<div class="layer description"><img src="<?php echo $this->appAssets('images/arm/junzhi-text.png')?>"></div>
+	<div class="layer description">
+        <p>按要求先行完成军职者，不论出身，部分贵贱，封<a href="#jiangjunxian-dialog" class="fancybox-inline">“将军衔”</a>，授<a href="#yunchangjian-dialog" class="fancybox-inline">“云长剑”</a>（关羽军团最高荣誉）。</p>
+        <p>千兵有头，万兵有将。百名将军军衔等着授封，记住，只有百名！</p>
+    </div>
+</div>
+<div class="hide">
+    <div id="jiangjunxian-dialog" class="dialog">
+        <div class="dialog-content">
+            <h4>关羽军团最高军职</h4>
+            <h5>履职有功者 授封将军衔</h5>
+            <img src="<?php echo $this->appAssets('images/arm/jiangjunxian.png')?>">
+            <div class="desc">
+                <p>
+                    <label>权属</label>
+                    <span>建安24年，刘备进位汉中王，拜关羽为前将军、假节钺，可代主行使最高权力，全权董督荆州事。</span>
+                </p>
+                <p>
+                    <label>规格</label>
+                    <span>长7厘米 宽5.5厘米 厚0.3厘米</span>
+                </p>
+                <p>
+                    <label>品质</label>
+                    <span>纯银做旧</span>
+                </p>
+                <p>
+                    <label>个性</label>
+                    <span>专属刻名</span>
+                </p>
+                <p>
+                    <label>送达</label>
+                    <span>快递公司指定送达</span>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="hide">
+    <div id="yunchangjian-dialog" class="dialog">
+        <div class="dialog-content">
+            <h4>关羽军团最高荣誉</h4>
+            <h5>领将军衔 佩云长剑</h5>
+            <img src="<?php echo $this->appAssets('images/arm/yunchangjian.jpg')?>">
+            <div class="desc">
+                <p>总长度 108cm  总重量 2000g  剑刃重 1100g</p>
+                <p>
+                    <label>剑鞘用料</label>
+                    <span>黑檀木实木</span>
+                </p>
+                <p>
+                    <label>装具用料</label>
+                    <span>加厚纯铜，双层雕刻，手工打磨做旧（防锈）</span>
+                </p>
+                <p>
+                    <label>锻打工艺</label>
+                    <span>八面剑刃，11段折叠锻打百炼花纹钢（锻纹密度4000层），13道全手工超细研磨（镜面效果），古法覆土烧刃（硬度61－62）</span>
+                </p>
+                <p>
+                    <label>标配礼盒</label>
+                    <span>精装木框缎面锦盒，专用剑架，绸布剑袋，养护刀油，擦刀布，使用手册，防伪标签。</span>
+                </p>
+                <p>
+                    <label>宝剑品牌</label>
+                    <span>四百年古法铸剑历史之嵩山宝剑</span>
+                </p>
+                <p>
+                    <label>独有个性</label>
+                    <span>专属定制，持有刻名</span>
+                </p>
+                <p>
+                    <label>快递送达</label>
+                    <span>快递公司安全送达</span>
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
 <script type="text/javascript" src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 <script>
@@ -59,6 +133,16 @@ wx.ready(function(){
 		}
 	});
 });
+
+$('#post-dialog').css({'width': document.documentElement.clientWidth * 0.7});
+$('#yunchangjian-dialog').css({
+	'width': document.documentElement.clientWidth * 0.75,
+	'height': document.documentElement.clientHeight * 0.8
+});
+$('#jiangjunxian-dialog').css({
+	'width': document.documentElement.clientWidth * 0.75
+});
+
 $(function(){
 	$('.task-link').on('click', function(){
 		$('body').block();

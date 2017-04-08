0d50f22dfb56f2c5a23eb687f47461d3617b7258
fayfox <admin@fayfox.com>
2017-04-08 22:00:32 +0800

    重新架构后各种改

--- application/pharmrich/configs/routes.php
+++ application/pharmrich/configs/routes.php
@@ -1,22 +1,16 @@
 <?php
 return array(
-	/**
-	 * 隐藏真实后台
-	 */
-	'/^admin$/'=>'404',
-	'/^admin\/$/'=>'404',
+	'/^product\/(\d+)$/'=>'product/item:id=$1',
+	'/^product\/([\w-]+)$/'=>'product/index:cat_alias=$1',
 	
-	'/^product\/(\d+)$/'=>'product/item/id/$1',
-	'/^product\/([\w-]+)$/'=>'product/index/cat_alias/$1',
+	'/^cook-recipe\/(\d+)$/'=>'cook-recipe/item:id=$1',
+	'/^cook-recipe\/([\w-]+)$/'=>'cook-recipe/index:cat_alias=$1',
 	
-	'/^cook-recipe\/(\d+)$/'=>'cook-recipe/item/id/$1',
-	'/^cook-recipe\/([\w-]+)$/'=>'cook-recipe/index/cat_alias/$1',
-	
-	'/^news\/(\d+)$/'=>'news/item/id/$1',
-	'/^news\/([\w-]+)$/'=>'news/index/cat_alias/$1',
+	'/^news\/(\d+)$/'=>'news/item:id=$1',
+	'/^news\/([\w-]+)$/'=>'news/index:cat_alias=$1',
 
-	'/^about$/'=>'page/item/alias/about',
-	'/^shipment$/'=>'page/item/alias/shipment',
-	'/^faq$/'=>'page/item/alias/faq',
-	'/^order$/'=>'page/item/alias/order',
+	'/^about$/'=>'page/item:alias=about',
+	'/^shipment$/'=>'page/item:alias=shipment',
+	'/^faq$/'=>'page/item:alias=faq',
+	'/^order$/'=>'page/item:alias=order',
 );

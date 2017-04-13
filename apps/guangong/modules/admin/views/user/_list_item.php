bfda462e39f3f60f1c9c744b5a4f53c44308f9e3
fayfox <admin@fayfox.com>
2017-04-13 14:31:28 +0800

    关公点兵

---
+++ apps/guangong/modules/admin/views/user/_list_item.php
@@ -0,0 +1,133 @@
+<?php
+use cms\models\tables\UsersTable;
+use fay\helpers\DateHelper;
+use fay\helpers\HtmlHelper;
+use cms\services\file\FileService;
+use cms\services\user\UserRoleService;
+?>
+<tr valign="top" id="user-<?php echo $data['id']?>">
+    <?php if(in_array('avatar', $cols)){?>
+    <td><?php echo HtmlHelper::link(HtmlHelper::img($data['avatar'], FileService::PIC_THUMBNAIL, array(
+        'width'=>40,
+        'height'=>40,
+        'class'=>'circle',
+        'spare'=>'avatar',
+    )), array('cms/admin/user/item', array(
+        'id'=>$data['id'],
+    )), array(
+        'title'=>false,
+        'encode'=>false,
+    ))?></td>
+    <?php }?>
+    
+    <td>
+        <strong>
+            <?php if($data['nickname'])
+                echo HtmlHelper::encode($data['nickname']);
+            else
+                echo '&nbsp;';?>
+        </strong>
+        <div class="row-actions">
+            <?php
+                echo HtmlHelper::link('查看', array('cms/admin/user/item', array(
+                    'id'=>$data['id'],
+                )), array(), true);
+                echo HtmlHelper::link('编辑', array('cms/admin/user/edit', array(
+                    'id'=>$data['id'],
+                )), array(), true);
+            ?>
+        </div>
+    </td>
+    
+    <?php if(in_array('roles', $cols)){?>
+    <td><?php
+        $user_roles = UserRoleService::service()->get($data['id']);
+        foreach($user_roles as $k => $role){
+            if($k){
+                echo ', ';
+            }
+            echo HtmlHelper::link($role['title'], array('cms/admin/user/index', array(
+                'role'=>$role['id']
+            )));
+        }
+    ?></td>
+    <?php }?>
+    
+    <?php if(in_array('mobile', $cols)){?>
+    <td><?php echo HtmlHelper::encode($data['mobile'])?></td>
+    <?php }?>
+    
+    <?php if(in_array('email', $cols)){?>
+    <td>
+        <a href="mailto:<?php echo HtmlHelper::encode($data['email'])?>"><?php echo HtmlHelper::encode($data['email'])?></a>
+    </td>
+    <?php }?>
+    
+    <?php if(in_array('status', $cols)){?>
+    <td>
+        <?php if($data['status'] == UsersTable::STATUS_PENDING){?>
+            <span class="fc-orange">未审核</span>
+        <?php }else if($data['status'] == UsersTable::STATUS_VERIFIED){?>
+            <span class="fc-green">通过审核</span>
+        <?php }else if($data['status'] == UsersTable::STATUS_VERIFY_FAILED){?>
+            <span class="fc-red">未通过审核</span>
+        <?php }else if($data['status'] == UsersTable::STATUS_UNCOMPLETED){?>
+            <span class="fc-orange">信息不完整</span>
+        <?php }else if($data['status'] == UsersTable::STATUS_NOT_VERIFIED){?>
+            <span class="fc-orange">未验证邮箱</span>
+        <?php }?>
+    </td>
+    <?php }?>
+    
+    <?php if(in_array('block', $cols)){?>
+    <td><?php if($data['block']){
+        echo '<span class="fc-red">阻塞</span>';
+    }else{
+        echo '<span class="fc-green">正常</span>';
+    }?></td>
+    <?php }?>
+    
+    <?php if(in_array('reg_time', $cols)){?>
+    <td>
+        <abbr class="time" title="<?php echo DateHelper::format($data['reg_time'])?>">
+            <?php echo DateHelper::niceShort($data['reg_time'])?>
+        </abbr>
+    </td>
+    <?php }?>
+    
+    <?php if(in_array('reg_ip', $cols)){?>
+    <td>
+        <abbr class="ip" title="<?php echo long2ip($data['reg_ip'])?>">
+            <?php echo $iplocation->getCountryAndArea(long2ip($data['reg_ip']))?>
+        </abbr>
+    </td>
+    <?php }?>
+    
+    <?php if(in_array('last_login_time', $cols)){?>
+    <td>
+        <abbr class="time" title="<?php echo DateHelper::format($data['last_login_time'])?>">
+            <?php echo DateHelper::niceShort($data['last_login_time'])?>
+        </abbr>
+    </td>
+    <?php }?>
+    
+    <?php if(in_array('last_login_ip', $cols)){?>
+    <td>
+        <abbr class="ip" title="<?php echo long2ip($data['last_login_ip'])?>">
+            <?php echo $iplocation->getCountryAndArea(long2ip($data['last_login_ip']))?>
+        </abbr>
+    </td>
+    <?php }?>
+    
+    <?php if(in_array('last_time_online', $cols)){?>
+    <td>
+        <span class="time abbr" title="<?php echo DateHelper::format($data['last_time_online'])?>">
+            <?php echo DateHelper::niceShort($data['last_time_online'])?>
+        </span>
+    </td>
+    <?php }?>
+    
+    <?php if(in_array('trackid', $cols)){?>
+    <td><?php echo $data['trackid']?></td>
+    <?php }?>
+</tr>
---
+++ apps/guangong/modules/admin/views/user/index.php
@@ -0,0 +1,139 @@
+<?php
+use fay\helpers\HtmlHelper;
+use cms\helpers\ListTableHelper;
+
+$cols = F::form('setting')->getData('cols');
+?>
+<div class="row">
+    <div class="col-12">
+        <?php echo F::form('search')->open(null, 'get', array(
+            'class'=>'form-inline',
+        ))?>
+            <div class="mb5">
+                <?php echo F::form('search')->select('time_field', array(
+                    'reg_time' =>'注册时间',
+                    'last_login_time' =>'最后登陆时间',
+                    'last_time_online' =>'最后活跃时间',
+                ), array(
+                    'class'=>'form-control',
+                ))?>
+                <?php echo F::form('search')->inputText('start_time', array(
+                    'data-rule'=>'datetime',
+                    'class'=>'datetimepicker form-control',
+                ));?>
+                -
+                <?php echo F::form('search')->inputText('end_time', array(
+                    'data-rule'=>'datetime',
+                    'class'=>'datetimepicker form-control',
+                ));?>
+            </div>
+            <div class="mb5">
+                <?php echo F::form('search')->select('keywords_field', array(
+                    'username'=>'登陆名',
+                    'nickname'=>'昵称',
+                    'mobile'=>'手机号',
+                    'email'=>'邮箱',
+                    'id'=>'用户ID',
+                ), array(
+                    'class'=>'form-control',
+                ))?>
+                <?php echo F::form('search')->inputText('keywords', array('class'=>'form-control w200'))?>
+                |
+                <?php echo F::form('search')->select('role', array(''=>'--角色--')+HtmlHelper::getSelectOptions($roles), array(
+                    'class'=>'form-control',
+                ));?>
+                <?php echo F::form('search')->submitLink('查询', array(
+                    'class'=>'btn btn-sm',
+                ))?>
+            </div>
+        <?php echo F::form('search')->close()?>
+        <?php $listview->showPager();?>
+        <table class="list-table">
+            <thead>
+                <tr>
+                    <?php if(in_array('avatar', $cols)){
+                        echo '<th class="w50">头像</th>';
+                    }?>
+                    <th>昵称</th>
+                    <?php if(in_array('roles', $cols)){
+                        echo '<th>角色</th>';
+                    }
+                    if(in_array('mobile', $cols)){
+                        echo '<th>手机</th>';
+                    }
+                    if(in_array('email', $cols)){
+                        echo '<th>邮箱</th>';
+                    }
+                    if(in_array('status', $cols)){
+                        echo '<th>状态</th>';
+                    }
+                    if(in_array('block', $cols)){
+                        echo '<th>阻塞</th>';
+                    }
+                    if(in_array('reg_time', $cols)){
+                        echo '<th>', ListTableHelper::getSortLink('reg_time', '注册时间'), '</th>';
+                    }
+                    if(in_array('reg_ip', $cols)){
+                        echo '<th>注册IP</th>';
+                    }
+                    if(in_array('last_login_time', $cols)){
+                        echo '<th>', ListTableHelper::getSortLink('last_login_time', '最后登陆时间'), '</th>';
+                    }
+                    if(in_array('last_login_ip', $cols)){
+                        echo '<th>最后登陆IP</th>';
+                    }
+                    if(in_array('last_time_online', $cols)){
+                        echo '<th>', ListTableHelper::getSortLink('last_time_online', '最后在线时间'), '</th>';
+                    }
+                    if(in_array('trackid', $cols)){
+                        echo '<th>Trackid</th>';
+                    }?>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                    <?php if(in_array('avatar', $cols)){
+                        echo '<th>头像</th>';
+                    }?>
+                    <th>昵称</th>
+                    <?php if(in_array('roles', $cols)){
+                        echo '<th>角色</th>';
+                    }
+                    if(in_array('mobile', $cols)){
+                        echo '<th>手机</th>';
+                    }
+                    if(in_array('email', $cols)){
+                        echo '<th>邮箱</th>';
+                    }
+                    if(in_array('block', $cols)){
+                        echo '<th>阻塞</th>';
+                    }
+                    if(in_array('reg_time', $cols)){
+                        echo '<th>', ListTableHelper::getSortLink('reg_time', '注册时间'), '</th>';
+                    }
+                    if(in_array('reg_ip', $cols)){
+                        echo '<th>注册IP</th>';
+                    }
+                    if(in_array('last_login_time', $cols)){
+                        echo '<th>', ListTableHelper::getSortLink('last_login_time', '最后登陆时间'), '</th>';
+                    }
+                    if(in_array('last_login_ip', $cols)){
+                        echo '<th>最后登陆IP</th>';
+                    }
+                    if(in_array('last_time_online', $cols)){
+                        echo '<th>', ListTableHelper::getSortLink('last_time_online', '最后在线时间'), '</th>';
+                    }
+                    if(in_array('trackid', $cols)){
+                        echo '<th>Trackid</th>';
+                    }?>
+                </tr>
+            </tfoot>
+            <tbody><?php
+                $listview->showData(array(
+                    'cols'=>$cols,
+                ));
+            ?></tbody>
+        </table>
+        <?php $listview->showPager();?>
+    </div>
+</div>

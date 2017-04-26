b03e15b1b9e4460ef52118fb1d487e5efe199a5d
fayfox <admin@fayfox.com>
2017-04-26 21:20:13 +0800

    steroid

--- apps/steroid/modules/frontend/views/index/widgets/ancillary_list.php
+++ apps/steroid/modules/frontend/views/index/widgets/ancillary_list.php
@@ -27,9 +27,9 @@ $cat = \cms\services\CategoryService::service()->get($widget->config['cat_id'], 
                     </div>
                     <div class="col-md-8 col-sm-8">
                         <div class="ancillary-info">
-                            <h3 class="title"><?php echo \fay\helpers\HtmlHelper::encode($p['post']['title'])?></h3>
+                            <h3 class="title"><?php echo \fay\helpers\HtmlHelper::link($p['post']['title'], array('post/'.$p['post']['id']))?></h3>
                             <h6 class="description"><?php echo isset($props['subtitle']['value']) ? $props['subtitle']['value'] : ''?></h6>
-                            <p class="text"><?php echo \fay\helpers\HtmlHelper::encode($p['post']['abstract'])?></p>
+                            <p class="text"><?php echo nl2br(\fay\helpers\HtmlHelper::encode($p['post']['abstract']))?></p>
                         </div>
                     </div>
                 </article>

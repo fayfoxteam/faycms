85a5f33bf646b363ddbeaaa0a80ce1bff26ab182
fayfox <admin@fayfox.com>
2017-05-11 17:59:22 +0800

    后台属性管理

--- vendor/faysoft/cms/modules/admin/views/prop/edit.php
+++ vendor/faysoft/cms/modules/admin/views/prop/edit.php
@@ -1,12 +1,15 @@
 <?php
 /**
  * @var $listview \fay\common\ListView
+ * @var $prop array
  */
 ?>
 <div class="row">
     <div class="col-5">
         <?php echo F::form()->open()?>
-            <?php $this->renderPartial('_edit_panel');?>
+            <?php $this->renderPartial('_edit_panel', array(
+                'prop'=>$prop
+            ));?>
             <div class="form-field">
                 <?php echo F::form()->submitLink('提交修改', array(
                     'class'=>'btn',

866d8f70d516d0e8a75bc944e0c135690031d637
fayfox <admin@fayfox.com>
2017-05-23 22:39:50 +0800

    baike

---
+++ apps/baike/modules/frontend/controllers/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+namespace baike\modules\frontend\controllers;
+
+use baike\library\FrontController;
+use cms\services\OptionService;
+
+class IndexController extends FrontController{
+    public function __construct(){
+        parent::__construct();
+        
+        $this->layout->title = OptionService::get('site:seo_index_title');
+        $this->layout->keywords = OptionService::get('site:seo_index_keywords');
+        $this->layout->description = OptionService::get('site:seo_index_description');
+        
+        $this->layout->current_directory = 'home';
+    }
+    
+    public function index(){
+        $this->view->render();
+    }
+    
+}
---
+++ apps/baike/modules/frontend/views/layouts/frontend.php
@@ -0,0 +1,38 @@
+<?php
+use cms\services\OptionService;
+use fay\helpers\HtmlHelper;
+
+/**
+ * @var $this \fay\core\View
+ * @var $content string
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<?php if(!empty($canonical)){?>
+<link rel="canonical" href="<?php echo $canonical?>" />
+<?php }?>
+<title><?php if(!empty($title)){
+    echo $title, '_';
+}
+echo OptionService::get('site:sitename')?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta content="<?php if(isset($keywords))echo HtmlHelper::encode($keywords);?>" name="keywords" />
+<meta content="<?php if(isset($description))echo HtmlHelper::encode($description);?>" name="description" />
+<link type="text/css" rel="stylesheet" href="<?php echo $this->appAssets('css/style.css')?>" >
+<?php echo $this->getCss()?>
+<script type="text/javascript" src="<?php echo $this->assets('js/jquery-1.8.3.min.js')?>"></script>
+<script type="text/javascript" src="<?php echo $this->assets('faycms/js/system.min.js')?>"></script>
+<script>
+system.base_url = '<?php echo $this->url()?>';
+system.user_id = '<?php echo \F::app()->current_user?>';
+</script>
+</head>
+<body>
+<div class="wrap">
+<?php echo $content?>
+</div>
+</body>
+</html>

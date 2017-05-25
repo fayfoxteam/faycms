23fe06fdca16ab51bdd962fc0e34bf0548c977cc
fayfox <admin@fayfox.com>
2017-05-25 13:45:06 +0800

    amq

--- apps/amq/modules/frontend/views/widget/carousel_ad.php
+++ apps/amq/modules/frontend/views/widget/carousel_ad.php
@@ -14,7 +14,7 @@
     <div class="carousel-inner" role="listbox">
         <?php foreach($files as $key => $file){?>
             <div class="item <?php if(!$key)echo 'active'?>">
-                <a href="<?php echo $file['link']?>"><img src="<?php echo $file['src']?>" alt="<?php echo \fay\helpers\HtmlHelper::encode($file['title'])?>"></a>
+                <a href="<?php echo $file['link']?>" target="_blank"><img src="<?php echo $file['src']?>" alt="<?php echo \fay\helpers\HtmlHelper::encode($file['title'])?>"></a>
             </div>
         <?php }?>
     </div>

703efb919c203a5abdca3ead57b9c639d4a80b0f
fayfox <admin@fayfox.com>
2017-06-01 20:27:01 +0800

    关公点兵

--- apps/guangong/modules/frontend/views/arm/index.php
+++ apps/guangong/modules/frontend/views/arm/index.php
@@ -21,7 +21,6 @@ $this->appendCss($this->appAssets('css/arm.css'));
             <div class="layer index-description"><img src="<?php echo $this->appAssets('images/arm/index-description.png')?>"></div>
         </div>
         <div class="swiper-slide" id="arm-2">
-            <div class="layer brand"><img src="<?php echo $this->appAssets('images/arm/brand.png')?>"></div>
             <div class="layer steps">
                 <?php if($hour){?>
                     <a href="#10" class="swiper-to" data-slide="10"><img src="<?php echo $this->appAssets('images/arm/s5.png')?>"></a>
@@ -36,10 +35,6 @@ $this->appendCss($this->appAssets('css/arm.css'));
                     <a href="#2" class="swiper-to" data-slide="2"><img src="<?php echo $this->appAssets('images/arm/s2.png')?>"></a>
                     <a href="#2" class="swiper-to" data-slide="2"><img src="<?php echo $this->appAssets('images/arm/s1.png')?>"></a>
                 <?php }?>
-            </div>
-            <div class="layer description">
-                <p>体验规则：</p>
-                <p>点击依序进行选择，以摇令定之；前四项确定后存入档案不可更改；第五项每天按规履行军职，最高可晋升至将军。</p>
             </div>
         </div>
         <?php

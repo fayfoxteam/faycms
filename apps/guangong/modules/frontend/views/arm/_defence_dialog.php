b48fb3f1120196856c82ccf9d166e84135d38c56
fayfox <admin@fayfox.com>
2017-06-18 09:39:03 +0800

    关公点兵

--- apps/guangong/modules/frontend/views/arm/_defence_dialog.php
+++ apps/guangong/modules/frontend/views/arm/_defence_dialog.php
@@ -6,7 +6,13 @@
 <div class="hide">
     <div id="defence-dialog" class="dialog">
         <div class="dialog-content">
-            <div class="defence-description"><?php echo \fay\helpers\HtmlHelper::img($defence['text_picture'])?></div>
+            <div class="defence-description"><?php
+                if(empty($defence['text_picture'])){
+                    echo \fay\helpers\HtmlHelper::img($defence['text_picture']);
+                }else{
+                    echo '<img src="">';
+                }
+            ?></div>
         </div>
     </div>
 </div>

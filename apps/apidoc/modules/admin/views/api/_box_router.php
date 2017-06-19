1e36b5718489418eaf386677c6e12e2e70e38609
fayfox <admin@fayfox.com>
2017-06-19 21:49:34 +0800

    移除app/apidoc代码，已经整合到faysoft了

--- apps/apidoc/modules/admin/views/api/_box_router.php
+++
@@ -1,9 +0,0 @@
-<div class="box" id="box-router" data-name="router">
-    <div class="box-title">
-        <a class="tools remove" title="隐藏"></a>
-        <h4>路由</h4>
-    </div>
-    <div class="box-content">
-        <?php echo F::form()->inputText('router', array('class'=>'form-control'))?>
-    </div>
-</div>

688f4345f8112d98ed6704c085c31bb131d0dfb6
fayfox <admin@fayfox.com>
2017-06-26 10:43:34 +0800

    修改是否处于编辑状态的判断逻辑

--- vendor/faysoft/cms/modules/admin/views/widget/customize.php
+++ vendor/faysoft/cms/modules/admin/views/widget/customize.php
@@ -27,27 +27,25 @@ html,body{overflow:hidden;height:100%}
 <script>
 var customize = {
     'initIframe': function(){
-        $('#customize-iframe').on('load', function(){
-            $(window.frames['customize-iframe'].document).find('body').append([
-                '<style>',
-                '.edit-widget-container{position:relative;transition:all 0.2s linear 0s}',
-                '.edit-widget-container .edit-widget-link{position:absolute;left:-30px;top:2px;color:#fff;width:30px;height:30px;font-size:18px;z-index:5;background-color:#0085ba;border-radius:50%;text-align:center;padding:3px;border:2px solid #fff;text-shadow:0 -1px 1px #006799, 1px 0 1px #006799, 0 1px 1px #006799, -1px 0 1px #006799;box-shadow:0 2px 1px rgba(46,68,83,.15);box-sizing:border-box}',
-                '.edit-widget-container .edit-widget-link svg{fill:#fff;width:20px;height:20px;margin:auto}',
-                '</style>'
-            ].join(''));
-            $(window.frames['customize-iframe'].document).on('click', '.edit-widget-link', function(){
-                parent.$.fancybox.open({
-                    'src': $(this).attr('data-src'),
-                    'type': 'iframe'
-                }, {
-                    'iframe': {
-                        'css': {
-                            'max-width': '1000px',
-                            'max-height': '88%'
-                        },
-                        'scrolling': 'yes'
-                    }
-                });
+        $(window.frames['customize-iframe'].document).find('body').append([
+            '<style>',
+            '.edit-widget-container{position:relative;transition:all 0.2s linear 0s}',
+            '.edit-widget-container .edit-widget-link{position:absolute;left:-30px;top:2px;color:#fff;width:30px;height:30px;font-size:18px;z-index:5;background-color:#0085ba;border-radius:50%;text-align:center;padding:3px;border:2px solid #fff;text-shadow:0 -1px 1px #006799, 1px 0 1px #006799, 0 1px 1px #006799, -1px 0 1px #006799;box-shadow:0 2px 1px rgba(46,68,83,.15);box-sizing:border-box}',
+            '.edit-widget-container .edit-widget-link svg{fill:#fff;width:20px;height:20px;margin:auto}',
+            '</style>'
+        ].join(''));
+        $(window.frames['customize-iframe'].document).on('click', '.edit-widget-link', function(){
+            parent.$.fancybox.open({
+                'src': $(this).attr('data-src'),
+                'type': 'iframe'
+            }, {
+                'iframe': {
+                    'css': {
+                        'max-width': '1000px',
+                        'max-height': '88%'
+                    },
+                    'scrolling': 'yes'
+                }
             });
         });
     },
@@ -81,13 +79,19 @@ var customize = {
             return false;
         });
     },
+    'showEditLink': function(){
+        $(window.frames['customize-iframe'].document).find('.edit-widget-link').show();
+    },
     'init': function(){
         this.initIframe();
         this.blockLink();
+        this.showEditLink();
     }
 };
 $(function(){
-    customize.init();
+    $('#customize-iframe').on('load', function(){
+        customize.init();
+    });
 });
 </script>
 </body>

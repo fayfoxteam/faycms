d59a12f5646fc92a6ab7f16912664dbba624b136
fayfox <admin@fayfox.com>
2017-06-30 15:36:45 +0800

    amq

--- apps/amq/configs/pagecache.php
+++ apps/amq/configs/pagecache.php
@@ -3,11 +3,11 @@
  * 页面缓存
  */
 return array(
-    'amq/frontend/index/index'=>array(
-        'params'=>array('page'),
-        'ttl'=>600,
-        'on_post'=>'noaction',//noaction无操作, rebuild重新创建, remove删除（不重建）
-    ),
+//    'amq/frontend/index/index'=>array(
+//        'params'=>array('page'),
+//        'ttl'=>600,
+//        'on_post'=>'noaction',//noaction无操作, rebuild重新创建, remove删除（不重建）
+//    ),
     'cms/api/site-map/xml'=>array(
         'ttl'=>86400 * 2,
     ),

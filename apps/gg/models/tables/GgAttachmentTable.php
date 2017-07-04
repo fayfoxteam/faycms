1fd5bf81305fedf3b892fe1f7e0bd7a28259a62a
fayfox <admin@fayfox.com>
2017-07-04 11:56:09 +0800

    gg

--- apps/gg/models/tables/GgAttachmentTable.php
+++ apps/gg/models/tables/GgAttachmentTable.php
@@ -15,6 +15,10 @@ use fay\core\Loader;
  * @property string $filepath Filepath
  * @property int $filetype Filetype
  * @property int $filesize Filesize
+ * @property int $is_image 是否为图片
+ * @property int $image_width 图片宽度
+ * @property int $image_height 图片高度
+ * @property string $mime_type Mime Type
  * @property int $qiniu 是否上传至七牛
  * @property string $updated_at 更新时间
  * @property string $created_at 创建时间
@@ -33,11 +37,13 @@ class GgAttachmentTable extends Table{
     public function rules(){
         return array(
             array(array('id'), 'int', array('min'=>0, 'max'=>4294967295)),
-            array(array('merchant_id', 'website_id', 'cat_id', 'filesize'), 'int', array('min'=>0, 'max'=>16777215)),
+            array(array('merchant_id', 'website_id', 'cat_id', 'filesize', 'image_width', 'image_height'), 'int', array('min'=>0, 'max'=>16777215)),
             array(array('qiniu'), 'int', array('min'=>-128, 'max'=>127)),
             array(array('filetype'), 'int', array('min'=>0, 'max'=>255)),
             array(array('title'), 'string', array('max'=>60)),
             array(array('filepath'), 'string', array('max'=>200)),
+            array(array('mime_type'), 'string', array('max'=>255)),
+            array(array('is_image'), 'range', array('range'=>array(0, 1))),
         );
     }
 
@@ -51,6 +57,10 @@ class GgAttachmentTable extends Table{
             'filepath'=>'Filepath',
             'filetype'=>'Filetype',
             'filesize'=>'Filesize',
+            'is_image'=>'是否为图片',
+            'image_width'=>'图片宽度',
+            'image_height'=>'图片高度',
+            'mime_type'=>'Mime Type',
             'qiniu'=>'是否上传至七牛',
             'updated_at'=>'更新时间',
             'created_at'=>'创建时间',
@@ -68,6 +78,10 @@ class GgAttachmentTable extends Table{
             'filepath'=>'trim',
             'filetype'=>'intval',
             'filesize'=>'intval',
+            'is_image'=>'intval',
+            'image_width'=>'intval',
+            'image_height'=>'intval',
+            'mime_type'=>'trim',
             'qiniu'=>'intval',
             'updated_at'=>'',
             'created_at'=>'',

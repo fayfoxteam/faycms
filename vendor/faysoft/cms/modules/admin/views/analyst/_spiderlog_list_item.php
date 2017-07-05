5695e5b299f6869e40574373476f7d9790320232
fayfox <admin@fayfox.com>
2017-07-05 10:01:13 +0800

    后台蜘蛛日志受访连接点击直接打开页面

--- vendor/faysoft/cms/modules/admin/views/analyst/_spiderlog_list_item.php
+++ vendor/faysoft/cms/modules/admin/views/analyst/_spiderlog_list_item.php
@@ -1,14 +1,17 @@
 <?php
 use fay\helpers\DateHelper;
 use fay\helpers\HtmlHelper;
-use fay\helpers\StringHelper;
 
+/**
+ * @var $data array
+ * @var $iplocation IpLocation
+ */
 ?>
 <tr>
     <td><?php echo $data['spider']?></td>
-    <td><?php echo HtmlHelper::link($data['url'], array('cms/admin/analyst/spiderlog', array(
-        'url'=>$data['url'],
-    ), false))?></td>
+    <td><?php echo HtmlHelper::link($data['url'], $data['url'], array(
+        'target'=>'_blank',
+    ))?></td>
     <td><abbr title="<?php echo $data['user_agent']?>"><?php echo $data['user_agent']?></abbr></td>
     <td><abbr title="<?php echo long2ip($data['ip_int'])?>"><?php echo $iplocation->getCountry(long2ip($data['ip_int']))?></abbr></td>
     <td class="col-time"><abbr title="<?php echo DateHelper::format($data['create_time'])?>"><?php echo DateHelper::format($data['create_time'])?></abbr></td>

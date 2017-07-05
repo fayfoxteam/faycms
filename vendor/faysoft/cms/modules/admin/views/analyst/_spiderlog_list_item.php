4a661df0bd57ab6c8943b3bae4f8d47d32c19f7c
fayfox <admin@fayfox.com>
2017-07-05 09:42:57 +0800

    后台蜘蛛日志不截断蜘蛛UA

--- vendor/faysoft/cms/modules/admin/views/analyst/_spiderlog_list_item.php
+++ vendor/faysoft/cms/modules/admin/views/analyst/_spiderlog_list_item.php
@@ -9,7 +9,7 @@ use fay\helpers\StringHelper;
     <td><?php echo HtmlHelper::link($data['url'], array('cms/admin/analyst/spiderlog', array(
         'url'=>$data['url'],
     ), false))?></td>
-    <td><abbr title="<?php echo $data['user_agent']?>"><?php echo StringHelper::niceShort($data['user_agent'], 50)?></abbr></td>
+    <td><abbr title="<?php echo $data['user_agent']?>"><?php echo $data['user_agent']?></abbr></td>
     <td><abbr title="<?php echo long2ip($data['ip_int'])?>"><?php echo $iplocation->getCountry(long2ip($data['ip_int']))?></abbr></td>
     <td class="col-time"><abbr title="<?php echo DateHelper::format($data['create_time'])?>"><?php echo DateHelper::format($data['create_time'])?></abbr></td>
 </tr>
